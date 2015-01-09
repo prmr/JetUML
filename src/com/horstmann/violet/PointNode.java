ca3c6243445097530546f2dbf625febd0e926076
prmr <prmr@gmx.com>
2015-01-08 22:02:26 -0500

    #8 Cleaned PointNode

--- src/com/horstmann/violet/PointNode.java
+++ src/com/horstmann/violet/PointNode.java
@@ -27,50 +27,50 @@ import java.awt.geom.Rectangle2D;
 import com.horstmann.violet.framework.AbstractNode;
 import com.horstmann.violet.framework.Direction;
 
-
 /**
-   An inivisible node that is used in the toolbar to draw an
-   edge, and in notes to serve as an end point of the node
-   connector.
-*/
+ *  An invisible node that is used in the toolbar to draw an
+ *  edge, and in notes to serve as an end point of the node
+ *  connector.
+ */
+@SuppressWarnings("serial")
 public class PointNode extends AbstractNode
 {
-	   private Point2D point;
+	private Point2D aPoint;
 
-	
-   /**
-      Constructs a point node with coordinates (0, 0)
-   */
-   public PointNode()
-   {
-      point = new Point2D.Double();
-   }
+	/**
+     * Constructs a point node with coordinates (0, 0).
+	 */
+	public PointNode()
+	{
+		aPoint = new Point2D.Double();
+	}
 
-   public void draw(Graphics2D g2)
-   {
-   }
+	@Override
+	public void draw(Graphics2D pGraphics2D)
+	{}
 
-   public void translate(double dx, double dy)
-   {
-      point.setLocation(point.getX() + dx,
-         point.getY() + dy);
-   }
+	@Override
+	public void translate(double pDeltaX, double pDeltaY)
+	{
+      aPoint.setLocation(aPoint.getX() + pDeltaX, aPoint.getY() + pDeltaY);
+	}
 
-   public boolean contains(Point2D p)
-   {
-      final double THRESHOLD = 5;
-      return point.distance(p) < THRESHOLD;
-   }
+	@Override
+	public boolean contains(Point2D pPoint)
+	{
+		final double threshold = 5;
+		return aPoint.distance(pPoint) < threshold;
+	}
 
-   public Rectangle2D getBounds()
-   {
-      return new Rectangle2D.Double(point.getX(), 
-         point.getY(), 0, 0);
-   }
+	@Override
+	public Rectangle2D getBounds()
+	{
+		return new Rectangle2D.Double(aPoint.getX(), aPoint.getY(), 0, 0);
+	}
 
-   public Point2D getConnectionPoint(Direction d)
-   {
-      return point;
-   }
-
+	@Override
+	public Point2D getConnectionPoint(Direction pDirection)
+	{
+		return aPoint;
+	}
 }
