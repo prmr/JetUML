6d7ff1dd189447272f7c4f80effef95934c7635a
prmr <prmr@gmx.com>
2015-01-09 11:08:15 -0500

    #8 CircularStateNode

--- src/com/horstmann/violet/CircularStateNode.java
+++ src/com/horstmann/violet/CircularStateNode.java
@@ -30,98 +30,101 @@ import com.horstmann.violet.framework.Direction;
 import com.horstmann.violet.framework.RectangularNode;
 
 /**
-   An initial or final node (bull's eye) in a state or activity diagram.
-*/
+ * An initial or final node (bull's eye) in a state or activity diagram.
+ */
+@SuppressWarnings("serial")
 public class CircularStateNode extends RectangularNode
 {
-	   private boolean finalState; // final is a keyword
+	private static final int DEFAULT_DIAMETER = 30;
+	private static final int DEFAULT_GAP = 4;   
+	
+	private boolean aFinalState; 
 	   
-	   private static int DEFAULT_DIAMETER = 30;
-	   private static int DEFAULT_GAP = 4;   
-	
-	
-   /**
-      Construct a node with a default size
-   */
-   public CircularStateNode()
-   {     
-      setBounds(new Rectangle2D.Double(0, 0,
-            DEFAULT_DIAMETER, DEFAULT_DIAMETER));      
-   }
+	/**
+	 * Construct a node with a default size.
+	 */
+	public CircularStateNode()
+	{     
+		setBounds(new Rectangle2D.Double(0, 0, DEFAULT_DIAMETER, DEFAULT_DIAMETER));      
+	}
    
-   public boolean isFinal()
-   {
-      return finalState; 
-   }
+	/**
+	 * @return true if this represents a final state.
+	 */
+	public boolean isFinal()
+	{
+		return aFinalState; 
+	}
    
-   public void setFinal(boolean newValue)
-   {
-      finalState = newValue;
-      Rectangle2D bounds = getBounds();
-      double x = bounds.getX();
-      double y = bounds.getY();
+	/**
+	 * @param pFinalState true to set this object to represent a final state.
+	 */
+	public void setFinal(boolean pFinalState)
+	{
+		aFinalState = pFinalState;
+		Rectangle2D bounds = getBounds();
+		double x = bounds.getX();
+		double y = bounds.getY();
       
-      if (finalState)
-         setBounds(new Rectangle2D.Double(x - DEFAULT_GAP, y - DEFAULT_GAP,
+		if (aFinalState)
+		{
+			setBounds(new Rectangle2D.Double(x - DEFAULT_GAP, y - DEFAULT_GAP,
                DEFAULT_DIAMETER + 2 * DEFAULT_GAP, 
                DEFAULT_DIAMETER + 2 * DEFAULT_GAP));
-      else
-         setBounds(new Rectangle2D.Double(x + DEFAULT_GAP, y + DEFAULT_GAP,
+		}
+		else
+		{
+			setBounds(new Rectangle2D.Double(x + DEFAULT_GAP, y + DEFAULT_GAP,
                DEFAULT_DIAMETER, DEFAULT_DIAMETER));
-   }
+		}
+	}
    
-   public Point2D getConnectionPoint(Direction d)
-   {
-      Rectangle2D bounds = getBounds();
-      double a = bounds.getWidth() / 2;
-      double b = bounds.getHeight() / 2;
-      double x = d.getX();
-      double y = d.getY();
-      double cx = bounds.getCenterX();
-      double cy = bounds.getCenterY();
+	@Override
+	public Point2D getConnectionPoint(Direction pDirection)
+	{
+		Rectangle2D bounds = getBounds();
+		double a = bounds.getWidth() / 2;
+		double b = bounds.getHeight() / 2;
+		double x = pDirection.getX();
+		double y = pDirection.getY();
+		double cx = bounds.getCenterX();
+		double cy = bounds.getCenterY();
       
-      if (a != 0 && b != 0 && !(x == 0 && y == 0))
-      {
-         double t = Math.sqrt((x * x) / (a * a) + (y * y) / (b * b));
-         return new Point2D.Double(cx + x / t, cy + y / t);
-      }
-      else
-      {
-         return new Point2D.Double(cx, cy);
-      }
-   }    
+		if(a != 0 && b != 0 && !(x == 0 && y == 0))
+		{
+			double t = Math.sqrt((x * x) / (a * a) + (y * y) / (b * b));
+			return new Point2D.Double(cx + x / t, cy + y / t);
+		}
+		else
+		{
+			return new Point2D.Double(cx, cy);
+		}
+	}   	 
 
-   public void draw(Graphics2D g2)
-   {
-      super.draw(g2);
-      Ellipse2D circle
-      = new Ellipse2D.Double(
-            getBounds().getX(), getBounds().getY(),
-            getBounds().getWidth(), getBounds().getHeight());
+	@Override
+	public void draw(Graphics2D pGraphics2D)
+	{
+		super.draw(pGraphics2D);
+		Ellipse2D circle = new Ellipse2D.Double(getBounds().getX(), getBounds().getY(), getBounds().getWidth(), getBounds().getHeight());
       
-      if (finalState)
-      {
-         Rectangle2D bounds = getBounds();
-         Ellipse2D inside
-            = new Ellipse2D.Double(
-               bounds.getX() + DEFAULT_GAP,
-               bounds.getY() + DEFAULT_GAP,
-               bounds.getWidth() - 2 * DEFAULT_GAP,
-               bounds.getHeight() - 2 * DEFAULT_GAP);
-         g2.fill(inside);
-         g2.draw(circle);
-      }
-      else
-         g2.fill(circle);      
-   }
+      	if(aFinalState)
+      	{
+      		Rectangle2D bounds = getBounds();
+      		Ellipse2D inside = new Ellipse2D.Double( bounds.getX() + DEFAULT_GAP, 
+      				bounds.getY() + DEFAULT_GAP, bounds.getWidth() - 2 * DEFAULT_GAP, bounds.getHeight() - 2 * DEFAULT_GAP);
+      		pGraphics2D.fill(inside);
+      		pGraphics2D.draw(circle);
+      	}
+		else
+		{
+			pGraphics2D.fill(circle);
+		}      
+	}
    
-   public Shape getShape()
-   {
-      return new Ellipse2D.Double(
-            getBounds().getX(), getBounds().getY(),
-            getBounds().getWidth() - 1, getBounds().getHeight() - 1);
-   }
-
-
+	@Override
+	public Shape getShape()
+	{
+		return new Ellipse2D.Double(getBounds().getX(), getBounds().getY(), getBounds().getWidth() - 1, getBounds().getHeight() - 1);
+	}
 }
 
