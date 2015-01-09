4cc9d506e6a21b39b53b5dc1e13aa105ff061bd0
prmr <prmr@gmx.com>
2015-01-09 11:17:09 -0500

    #8 ClassNode

--- src/com/horstmann/violet/ClassNode.java
+++ src/com/horstmann/violet/ClassNode.java
@@ -31,155 +31,150 @@ import com.horstmann.violet.framework.Node;
 import com.horstmann.violet.framework.RectangularNode;
 
 /**
-   A class node in a class diagram.
-*/
+ * A class node in a class diagram.
+ */
+@SuppressWarnings("serial")
 public class ClassNode extends RectangularNode
 {
-	  private double midHeight;
-	   private double botHeight;
-	   private MultiLineString name;
-	   private MultiLineString attributes;
-	   private MultiLineString methods;
-
-	   private static int DEFAULT_COMPARTMENT_HEIGHT = 20;
-	   private static int DEFAULT_WIDTH = 100;
-	   private static int DEFAULT_HEIGHT = 60;
+	private static final int DEFAULT_COMPARTMENT_HEIGHT = 20;
+	private static final int DEFAULT_WIDTH = 100;
+	private static final int DEFAULT_HEIGHT = 60;
 	
-   /**
-      Construct a class node with a default size
-   */
-   public ClassNode()
-   {
-      name = new MultiLineString();
-      name.setSize(MultiLineString.LARGE);
-      attributes = new MultiLineString();
-      attributes.setJustification(MultiLineString.LEFT);
-      methods = new MultiLineString();
-      methods.setJustification(MultiLineString.LEFT);
-      setBounds(new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
-      midHeight = DEFAULT_COMPARTMENT_HEIGHT;
-      botHeight = DEFAULT_COMPARTMENT_HEIGHT;
+	private double aMidHeight;
+	private double aBottomHeight;
+	private MultiLineString aName;
+	private MultiLineString aAttributes;
+	private MultiLineString aMethods;
+
+	/**
+     * Construct a class node with a default size.
+	 */
+	public ClassNode()
+   	{
+	   aName = new MultiLineString();
+	   aName.setSize(MultiLineString.LARGE);
+	   aAttributes = new MultiLineString();
+	   aAttributes.setJustification(MultiLineString.LEFT);
+	   aMethods = new MultiLineString();
+	   aMethods.setJustification(MultiLineString.LEFT);
+	   setBounds(new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
+	   aMidHeight = DEFAULT_COMPARTMENT_HEIGHT;
+	   aBottomHeight = DEFAULT_COMPARTMENT_HEIGHT;
    }
 
-   public void draw(Graphics2D g2)
-   {
-      super.draw(g2);
-      Rectangle2D top = new Rectangle2D.Double(getBounds().getX(),
-         getBounds().getY(), getBounds().getWidth(), 
-         getBounds().getHeight() - midHeight - botHeight);
-      g2.draw(top);
-      name.draw(g2, top);
-      Rectangle2D mid = new Rectangle2D.Double(top.getX(),
-         top.getMaxY(), top.getWidth(), midHeight);
-      g2.draw(mid);
-      attributes.draw(g2, mid);
-      Rectangle2D bot = new Rectangle2D.Double(top.getX(),
-         mid.getMaxY(), top.getWidth(), botHeight);
-      g2.draw(bot);
-      methods.draw(g2, bot);
+	@Override
+	public void draw(Graphics2D pGraphics2D)
+	{
+		super.draw(pGraphics2D);
+		Rectangle2D top = new Rectangle2D.Double(getBounds().getX(), getBounds().getY(), 
+				getBounds().getWidth(), getBounds().getHeight() - aMidHeight - aBottomHeight);
+		pGraphics2D.draw(top);
+		aName.draw(pGraphics2D, top);
+		Rectangle2D mid = new Rectangle2D.Double(top.getX(), top.getMaxY(), top.getWidth(), aMidHeight);
+		pGraphics2D.draw(mid);
+		aAttributes.draw(pGraphics2D, mid);
+		Rectangle2D bot = new Rectangle2D.Double(top.getX(), mid.getMaxY(), top.getWidth(), aBottomHeight);
+		pGraphics2D.draw(bot);
+		aMethods.draw(pGraphics2D, bot);
    }
 
-   public void layout(Graph g, Graphics2D g2, Grid grid)
-   {
-      Rectangle2D min = new Rectangle2D.Double(0, 0,
-         DEFAULT_WIDTH, DEFAULT_COMPARTMENT_HEIGHT);
-      Rectangle2D top = name.getBounds(g2); 
-      top.add(min);
-      Rectangle2D mid = attributes.getBounds(g2);
-      Rectangle2D bot = methods.getBounds(g2);
+	@Override
+	public void layout(Graph pGraph, Graphics2D pGraphics2D, Grid pGrid)
+	{
+		Rectangle2D min = new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_COMPARTMENT_HEIGHT);
+		Rectangle2D top = aName.getBounds(pGraphics2D); 
+		top.add(min);
+		Rectangle2D mid = aAttributes.getBounds(pGraphics2D);
+		Rectangle2D bot = aMethods.getBounds(pGraphics2D);
 
-      midHeight = mid.getHeight();
-      botHeight = bot.getHeight();
-      if (midHeight == 0 && botHeight == 0)
-      {
-         top.add(new Rectangle2D.Double(0, 0, 
-                    DEFAULT_WIDTH, 
-                    DEFAULT_HEIGHT));
-      }
-      else
-      {
-         mid.add(min);
-         bot.add(min);
-         midHeight = mid.getHeight();
-         botHeight = bot.getHeight();
-      }
+		aMidHeight = mid.getHeight();
+		aBottomHeight = bot.getHeight();
+		if(aMidHeight == 0 && aBottomHeight == 0)
+		{
+			top.add(new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
+		}
+		else
+		{
+			mid.add(min);
+			bot.add(min);
+			aMidHeight = mid.getHeight();
+			aBottomHeight = bot.getHeight();
+		}
 
-      Rectangle2D b = new Rectangle2D.Double(
-         getBounds().getX(), getBounds().getY(),
-         Math.max(top.getWidth(), Math.max(mid.getWidth(),
-            bot.getWidth())), 
-         top.getHeight() + midHeight + botHeight);
-      grid.snap(b);
-      setBounds(b);
-   }
+		Rectangle2D b = new Rectangle2D.Double(getBounds().getX(), getBounds().getY(), 
+				Math.max(top.getWidth(), Math.max(mid.getWidth(), bot.getWidth())), 
+				top.getHeight() + aMidHeight + aBottomHeight);
+		pGrid.snap(b);
+		setBounds(b);
+	}
 
-   public boolean addNode(Node n, Point2D p)
-   {
-      return n instanceof PointNode;
-   }
+	@Override
+	public boolean addNode(Node pNode, Point2D pPoint)
+	{
+		return pNode instanceof PointNode;
+	}
 
-   /**
-      Sets the name property value.
-      @param newValue the class name
-   */
-   public void setName(MultiLineString newValue)
-   {
-      name = newValue;
-   }
+	/**
+     * Sets the name property value.
+     * @param pName the class name
+	 */
+	public void setName(MultiLineString pName)
+	{
+		aName = pName;
+	}
 
-   /**
-      Gets the name property value.
-      @return the class name
-   */
-   public MultiLineString getName()
-   {
-      return name;
-   }
+	/**
+     * Gets the name property value.
+     * @return the class name
+	 */
+	public MultiLineString getName()
+	{
+		return aName;
+	}
 
-   /**
-      Sets the attributes property value.
-      @param newValue the attributes of this class
-   */
-   public void setAttributes(MultiLineString newValue)
-   {
-      attributes = newValue;
-   }
+	/**
+     * Sets the attributes property value.
+     * @param pNewValue the attributes of this class
+	 */
+	public void setAttributes(MultiLineString pNewValue)
+	{
+		aAttributes = pNewValue;
+	}
 
-   /**
-      Gets the attributes property value.
-      @return the attributes of this class
-   */
-   public MultiLineString getAttributes()
-   {
-      return attributes;
-   }
+	/**
+     * Gets the attributes property value.
+     * @return the attributes of this class
+	 */
+	public MultiLineString getAttributes()
+	{
+		return aAttributes;
+	}
 
-   /**
-      Sets the methods property value.
-      @param newValue the methods of this class
-   */
-   public void setMethods(MultiLineString newValue)
-   {
-      methods = newValue;
-   }
+	/**
+     * Sets the methods property value.
+     * @param pNewValue the methods of this class
+	 */
+	public void setMethods(MultiLineString pNewValue)
+	{
+		aMethods = pNewValue;
+	}
 
-   /**
-      Gets the methods property value.
-      @return the methods of this class
-   */
-   public MultiLineString getMethods()
-   {
-      return methods;
-   }
+	/**
+     * Gets the methods property value.
+     * @return the methods of this class
+	 */
+	public MultiLineString getMethods()
+	{
+		return aMethods;
+	}
 
-   public ClassNode clone()
-   {
-      ClassNode cloned = (ClassNode)super.clone();
-      cloned.name = (MultiLineString)name.clone();
-      cloned.methods = (MultiLineString)methods.clone();
-      cloned.attributes = (MultiLineString)attributes.clone();
-      return cloned;
-   }
-
- 
+	@Override
+	public ClassNode clone()
+	{
+		ClassNode cloned = (ClassNode)super.clone();
+		cloned.aName = (MultiLineString)aName.clone();
+		cloned.aMethods = (MultiLineString)aMethods.clone();
+		cloned.aAttributes = (MultiLineString)aAttributes.clone();
+		return cloned;
+	}
 }
