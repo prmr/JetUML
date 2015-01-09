2865a2be67d81d035313de5e672ddc0a4b29e314
prmr <prmr@gmx.com>
2015-01-08 22:09:13 -0500

    #8 RectangularNode

--- src/com/horstmann/violet/framework/RectangularNode.java
+++ src/com/horstmann/violet/framework/RectangularNode.java
@@ -24,149 +24,150 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.RectangularShape;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 
 /**
-   A node that has a rectangular shape.
-*/
+ * A node that has a rectangular shape.
+ */
+@SuppressWarnings("serial")
 public abstract class RectangularNode extends AbstractNode
 {
-	   private transient Rectangle2D bounds;
+	private transient Rectangle2D aBounds;
 
-	
-   public RectangularNode clone()
-   {
-      RectangularNode cloned = (RectangularNode)super.clone();
-      cloned.bounds = (Rectangle2D)bounds.clone();
-      return cloned;
-   }
+	@Override
+	public RectangularNode clone()
+	{
+		RectangularNode cloned = (RectangularNode) super.clone();
+		cloned.aBounds = (Rectangle2D) aBounds.clone();
+		return cloned;
+	}
 
-   public void translate(double dx, double dy)
-   {
-      bounds.setFrame(bounds.getX() + dx,
-         bounds.getY() + dy, 
-         bounds.getWidth(), 
-         bounds.getHeight());
-      super.translate(dx, dy);
-   }
+	@Override
+	public void translate(double pDeltaX, double pDeltaY)
+	{
+      aBounds.setFrame(aBounds.getX() + pDeltaX, aBounds.getY() + pDeltaY, aBounds.getWidth(), aBounds.getHeight());
+      super.translate(pDeltaX, pDeltaY);
+	}
 
-   public boolean contains(Point2D p)
-   {
-      return bounds.contains(p);
-   }
+	@Override
+	public boolean contains(Point2D pPoint)
+	{
+		return aBounds.contains(pPoint);
+	}
 
-   public Rectangle2D getBounds()
-   {
-      return (Rectangle2D)bounds.clone();
-   }
+	@Override
+	public Rectangle2D getBounds()
+	{
+		return (Rectangle2D) aBounds.clone();
+	}
 
-   public void setBounds(Rectangle2D newBounds)
-   {
-      bounds = newBounds;
-   }
+	/**
+	 * @param pNewBounds The new bounds for this node.
+	 */
+	public void setBounds(Rectangle2D pNewBounds)
+	{
+		aBounds = pNewBounds;
+	}
 
-   public void layout(Graph g, Graphics2D g2, Grid grid)
-   {
-      grid.snap(bounds);
-   }
+	@Override
+	public void layout(Graph pGraph, Graphics2D pGraphics2D, Grid pGrid)
+	{
+		pGrid.snap(aBounds);
+	}
 
-   public Point2D getConnectionPoint(Direction d)
-   {
-      double slope = bounds.getHeight() / bounds.getWidth();
-      double ex = d.getX();
-      double ey = d.getY();
-      double x = bounds.getCenterX();
-      double y = bounds.getCenterY();
+	@Override
+	public Point2D getConnectionPoint(Direction pDirection)
+	{
+		double slope = aBounds.getHeight() / aBounds.getWidth();
+		double ex = pDirection.getX();
+		double ey = pDirection.getY();
+		double x = aBounds.getCenterX();
+		double y = aBounds.getCenterY();
       
-      if (ex != 0 && -slope <= ey / ex && ey / ex <= slope)
-      {  
-         // intersects at left or right boundary
-         if (ex > 0) 
-         {
-            x = bounds.getMaxX();
-            y += (bounds.getWidth() / 2) * ey / ex;
-         }
-         else
-         {
-            x = bounds.getX();
-            y -= (bounds.getWidth() / 2) * ey / ex;
-         }
-      }
-      else if (ey != 0)
-      {  
-         // intersects at top or bottom
-         if (ey > 0) 
-         {
-            x += (bounds.getHeight() / 2) * ex / ey;
-            y = bounds.getMaxY();
-         }
-         else
-         {
-            x -= (bounds.getHeight() / 2) * ex / ey;
-            y = bounds.getY();
-         }
-      }
-      return new Point2D.Double(x, y);
-   }
+		if(ex != 0 && -slope <= ey / ex && ey / ex <= slope)
+		{  
+			// intersects at left or right boundary
+			if(ex > 0) 
+			{
+				x = aBounds.getMaxX();
+				y += (aBounds.getWidth() / 2) * ey / ex;
+			}
+			else
+			{
+				x = aBounds.getX();
+				y -= (aBounds.getWidth() / 2) * ey / ex;
+			}
+		}
+		else if(ey != 0)
+		{  
+			// intersects at top or bottom
+			if(ey > 0) 
+			{
+				x += (aBounds.getHeight() / 2) * ex / ey;
+				y = aBounds.getMaxY();
+			}
+			else
+			{
+				x -= (aBounds.getHeight() / 2) * ex / ey;
+				y = aBounds.getY();
+			}
+		}
+		return new Point2D.Double(x, y);
+	}
 
-   private void writeObject(ObjectOutputStream out)
-      throws IOException
-   {
-      out.defaultWriteObject();
-      writeRectangularShape(out, bounds);
-   }
+//   private void writeObject(ObjectOutputStream out)
+//      throws IOException
+//   {
+//      out.defaultWriteObject();
+//      writeRectangularShape(out, aBounds);
+//   }
 
-   /**
-      A helper method to overcome the problem that the 2D shapes
-      aren't serializable. It writes x, y, width and height
-      to the stream.
-      @param out the stream
-      @param s the shape      
-   */
-   private static void writeRectangularShape(
-      ObjectOutputStream out, 
-      RectangularShape s)
-      throws IOException
-   {
-      out.writeDouble(s.getX());
-      out.writeDouble(s.getY());
-      out.writeDouble(s.getWidth());
-      out.writeDouble(s.getHeight());
-   }
+//   /**
+//      A helper method to overcome the problem that the 2D shapes
+//      aren't serializable. It writes x, y, width and height
+//      to the stream.
+//      @param out the stream
+//      @param s the shape      
+//   */
+//   private static void writeRectangularShape(
+//      ObjectOutputStream out, 
+//      RectangularShape s)
+//      throws IOException
+//   {
+//      out.writeDouble(s.getX());
+//      out.writeDouble(s.getY());
+//      out.writeDouble(s.getWidth());
+//      out.writeDouble(s.getHeight());
+//   }
 
-   private void readObject(ObjectInputStream in)
-      throws IOException, ClassNotFoundException
-   {
-      in.defaultReadObject();
-      bounds = new Rectangle2D.Double();
-      readRectangularShape(in, bounds);
-   }
+//   private void readObject(ObjectInputStream in)
+//      throws IOException, ClassNotFoundException
+//   {
+//      in.defaultReadObject();
+//      aBounds = new Rectangle2D.Double();
+//      readRectangularShape(in, aBounds);
+//   }
    
-   /**
-      A helper method to overcome the problem that the 2D shapes
-      aren't serializable. It reads x, y, width and height
-      from the stream.
-      @param in the stream
-      @param s the shape whose frame is set from the stream values
-   */
-   private static void readRectangularShape(ObjectInputStream in,
-      RectangularShape s)
-      throws IOException
-   {
-      double x = in.readDouble();
-      double y = in.readDouble();
-      double width = in.readDouble();
-      double height = in.readDouble();
-      s.setFrame(x, y, width, height);
-   }
+//   /**
+//      A helper method to overcome the problem that the 2D shapes
+//      aren't serializable. It reads x, y, width and height
+//      from the stream.
+//      @param in the stream
+//      @param s the shape whose frame is set from the stream values
+//   */
+//   private static void readRectangularShape(ObjectInputStream in,
+//      RectangularShape s)
+//      throws IOException
+//   {
+//      double x = in.readDouble();
+//      double y = in.readDouble();
+//      double width = in.readDouble();
+//      double height = in.readDouble();
+//      s.setFrame(x, y, width, height);
+//   }
 
-   public Shape getShape()
-   {
-      return bounds;
-   }
-   
+	@Override
+	public Shape getShape()
+	{
+		return aBounds;
+	}
 }
