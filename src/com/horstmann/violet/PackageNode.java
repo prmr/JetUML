f8615a9db37361c6340378186ec611ae0793849d
prmr <prmr@gmx.com>
2015-01-10 08:49:34 -0500

    #8 PackageNode

--- src/com/horstmann/violet/PackageNode.java
+++ src/com/horstmann/violet/PackageNode.java
@@ -37,173 +37,167 @@ import com.horstmann.violet.framework.Node;
 import com.horstmann.violet.framework.RectangularNode;
 
 /**
-   A package node in a UML diagram.
-*/
+ *   A package node in a UML diagram.
+ */
+@SuppressWarnings("serial")
 public class PackageNode extends RectangularNode
 {
-
-	   private String name;
-	   private MultiLineString contents;
-
-	   private Rectangle2D top;
-	   private Rectangle2D bot;
+	private static final int DEFAULT_TOP_WIDTH = 60;
+	private static final int DEFAULT_TOP_HEIGHT = 20;
+	private static final int DEFAULT_WIDTH = 100;
+	private static final int DEFAULT_HEIGHT = 80;
+	private static final int NAME_GAP = 3;
+	private static final int XGAP = 5;
+	private static final int YGAP = 5;
 	   
-	   private static int DEFAULT_TOP_WIDTH = 60;
-	   private static int DEFAULT_TOP_HEIGHT = 20;
-	   private static int DEFAULT_WIDTH = 100;
-	   private static int DEFAULT_HEIGHT = 80;
-	   private static final int NAME_GAP = 3;
-	   private static final int XGAP = 5;
-	   private static final int YGAP = 5;
+	private static JLabel label = new JLabel();
+
+	private String aName;
+	private MultiLineString aContents;
+	private Rectangle2D aTop;
+	private Rectangle2D aBottom;
 	   
-	   private static JLabel label = new JLabel();
-	
-   /**
-      Construct a package node with a default size
-   */
-   public PackageNode()
-   {
-      name = "";
-      contents = new MultiLineString();
-      setBounds(new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
-      top = new Rectangle2D.Double(0, 0, DEFAULT_TOP_WIDTH, DEFAULT_TOP_HEIGHT);
-      bot = new Rectangle2D.Double(0, DEFAULT_TOP_HEIGHT, DEFAULT_WIDTH, DEFAULT_HEIGHT - DEFAULT_TOP_HEIGHT);
-   }
+	/**
+     * Construct a package node with a default size.
+	 */
+	public PackageNode()
+	{
+		aName = "";
+		aContents = new MultiLineString();
+		setBounds(new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
+		aTop = new Rectangle2D.Double(0, 0, DEFAULT_TOP_WIDTH, DEFAULT_TOP_HEIGHT);
+		aBottom = new Rectangle2D.Double(0, DEFAULT_TOP_HEIGHT, DEFAULT_WIDTH, DEFAULT_HEIGHT - DEFAULT_TOP_HEIGHT);
+	}
 
-   public void draw(Graphics2D g2)
-   {
-      super.draw(g2);
-      Rectangle2D bounds = getBounds();
+	@Override
+	public void draw(Graphics2D pGraphics2D)
+	{
+		super.draw(pGraphics2D);
+		Rectangle2D bounds = getBounds();
 
-      label.setText("<html>" + name + "</html>");
-      label.setFont(g2.getFont());
-      Dimension d = label.getPreferredSize();
-      label.setBounds(0, 0, d.width, d.height);
+		label.setText("<html>" + aName + "</html>");
+		label.setFont(pGraphics2D.getFont());
+		Dimension d = label.getPreferredSize();
+		label.setBounds(0, 0, d.width, d.height);
 
-      g2.draw(top);
+		pGraphics2D.draw(aTop);
 
-      double textX = bounds.getX() + NAME_GAP;
-      double textY = bounds.getY() + (top.getHeight() - d.getHeight()) / 2;
+		double textX = bounds.getX() + NAME_GAP;
+		double textY = bounds.getY() + (aTop.getHeight() - d.getHeight()) / 2;
       
-      g2.translate(textX, textY);
-      label.paint(g2);
-      g2.translate(-textX, -textY);        
+		pGraphics2D.translate(textX, textY);
+		label.paint(pGraphics2D);
+		pGraphics2D.translate(-textX, -textY);        
      
-      g2.draw(bot);
-      contents.draw(g2, bot);
-   }
+		pGraphics2D.draw(aBottom);
+		aContents.draw(pGraphics2D, aBottom);
+	}
    
-   public Shape getShape()
-   {
-      GeneralPath path = new GeneralPath();
-      path.append(top, false);
-      path.append(bot, false);
-      return path;
-   }
+	@Override
+	public Shape getShape()
+	{
+		GeneralPath path = new GeneralPath();
+		path.append(aTop, false);
+		path.append(aBottom, false);
+		return path;
+	}
 
-   public void layout(Graph g, Graphics2D g2, Grid grid)
-   {
-      Rectangle2D bounds = getBounds();
+	@Override
+	public void layout(Graph pGraph, Graphics2D pGraphics2D, Grid pGrid)
+	{
+		Rectangle2D bounds = getBounds();
 
-      label.setText("<html>" + name + "</html>");
-      label.setFont(g2.getFont());
-      Dimension d = label.getPreferredSize();
+		label.setText("<html>" + aName + "</html>");
+		label.setFont(pGraphics2D.getFont());
+		Dimension d = label.getPreferredSize();
       
-      top = new Rectangle2D.Double(bounds.getX(), bounds.getY(),
-         Math.max(d.getWidth(), DEFAULT_TOP_WIDTH), 
-         Math.max(d.getHeight(), DEFAULT_TOP_HEIGHT));
+		aTop = new Rectangle2D.Double(bounds.getX(), bounds.getY(), 
+				Math.max(d.getWidth(), DEFAULT_TOP_WIDTH), Math.max(d.getHeight(), DEFAULT_TOP_HEIGHT));
 
-      bot = contents.getBounds(g2);
-      Rectangle2D min = new Rectangle2D.Double(0, 0,
-         DEFAULT_WIDTH, DEFAULT_HEIGHT - DEFAULT_TOP_HEIGHT);
-      bot.add(min);
-      double width = Math.max(top.getWidth() + DEFAULT_WIDTH - DEFAULT_TOP_WIDTH, bot.getWidth());
-      double height = top.getHeight() + bot.getHeight();
+		aBottom = aContents.getBounds(pGraphics2D);
+		Rectangle2D min = new Rectangle2D.Double(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT - DEFAULT_TOP_HEIGHT);
+     	aBottom.add(min);
+     	double width = Math.max(aTop.getWidth() + DEFAULT_WIDTH - DEFAULT_TOP_WIDTH, aBottom.getWidth());
+     	double height = aTop.getHeight() + aBottom.getHeight();
 
-      List children = getChildren();
-      if (children.size() > 0)
-      {
-         Rectangle2D childBounds = new Rectangle2D.Double(
-            bounds.getX(), bounds.getY(), 0, 0);
-         for (int i = 0; i < children.size(); i++)
-         {
-            Node child = (Node)children.get(i);
-            child.layout(g, g2, grid);
-            childBounds.add(child.getBounds());
-         }
-         width = Math.max(width, 
-            childBounds.getWidth() + XGAP);
-         height = Math.max(height, 
-            childBounds.getHeight() + YGAP);
-      }
-      Rectangle2D b = new Rectangle2D.Double(
-         bounds.getX(), bounds.getY(), width, height);
-      grid.snap(b);
-      setBounds(b);
+     	List<Node> children = getChildren();
+     	if(children.size() > 0)
+     	{
+     		Rectangle2D childBounds = new Rectangle2D.Double(bounds.getX(), bounds.getY(), 0, 0);
+     		for(int i = 0; i < children.size(); i++)
+     		{
+     			Node child = children.get(i);
+     			child.layout(pGraph, pGraphics2D, pGrid);
+     			childBounds.add(child.getBounds());
+     		}
+     		width = Math.max(width, childBounds.getWidth() + XGAP);
+     		height = Math.max(height, childBounds.getHeight() + YGAP);
+     	}
+     	Rectangle2D b = new Rectangle2D.Double(bounds.getX(), bounds.getY(), width, height);
+     	pGrid.snap(b);
+     	setBounds(b);
       
-      top = new Rectangle2D.Double(
-            bounds.getX(), bounds.getY(),
-            Math.max(d.getWidth() + 2 * NAME_GAP, DEFAULT_TOP_WIDTH), 
-            Math.max(d.getHeight(), DEFAULT_TOP_HEIGHT));
+     	aTop = new Rectangle2D.Double(bounds.getX(), bounds.getY(), 
+     			Math.max(d.getWidth() + 2 * NAME_GAP, DEFAULT_TOP_WIDTH), Math.max(d.getHeight(), DEFAULT_TOP_HEIGHT));
       
-      bot = new Rectangle2D.Double(bounds.getX(),
-            bounds.getY() + top.getHeight(), 
-            bounds.getWidth(), bounds.getHeight() - top.getHeight());
+     	aBottom = new Rectangle2D.Double(bounds.getX(), bounds.getY() + aTop.getHeight(), bounds.getWidth(), bounds.getHeight() - aTop.getHeight());
+     }
+
+	/**
+     * Sets the name property value.
+     * @param pName the class name
+	 */
+	public void setName(String pName)
+	{
+		aName = pName;
+	}
+
+	/**
+     * Gets the name property value.
+     * @return the class name
+	 */
+	public String getName()
+	{
+		return aName;
+	}
+
+	/**
+     * Sets the contents property value.
+     * @param pContents the contents of this class
+	 */
+	public void setContents(MultiLineString pContents)
+	{
+		aContents = pContents;
+	}
+
+	/**
+     * Gets the contents property value.
+     * @return the contents of this class
+	 */
+	public MultiLineString getContents()
+	{
+		return aContents;
+	}
+
+	@Override
+	public PackageNode clone()
+	{
+		PackageNode cloned = (PackageNode)super.clone();
+		cloned.aContents = (MultiLineString)aContents.clone();
+		return cloned;
+	}
+
+	@Override
+	public boolean addNode(Node pNode, Point2D pPoint)
+	{
+		if(pNode instanceof ClassNode || pNode instanceof InterfaceNode || pNode instanceof PackageNode)
+		{
+			addChild(pNode);
+			return true;
+		}
+		else
+		{
+			return pNode instanceof NoteNode;
+		}
    }
-
-   /**
-      Sets the name property value.
-      @param newValue the class name
-   */
-   public void setName(String newValue)
-   {
-      name = newValue;
-   }
-
-   /**
-      Gets the name property value.
-      @return the class name
-   */
-   public String getName()
-   {
-      return name;
-   }
-
-   /**
-      Sets the contents property value.
-      @param newValue the contents of this class
-   */
-   public void setContents(MultiLineString newValue)
-   {
-      contents = newValue;
-   }
-
-   /**
-      Gets the contents property value.
-      @return the contents of this class
-   */
-   public MultiLineString getContents()
-   {
-      return contents;
-   }
-
-   public PackageNode clone()
-   {
-      PackageNode cloned = (PackageNode)super.clone();
-      cloned.contents = (MultiLineString)contents.clone();
-      return cloned;
-   }
-
-   public boolean addNode(Node n, Point2D p)
-   {
-      if (n instanceof ClassNode || n instanceof InterfaceNode || n instanceof PackageNode)
-      {
-         addChild(n);
-         return true;
-      }
-      else
-         return n instanceof NoteNode;
-   }
-
-
 }
