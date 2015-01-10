acabd7035ed1ce16536a0c319537d3b864931db3
prmr <prmr@gmx.com>
2015-01-10 13:28:14 -0500

    #8 Cleaned up Graph. Re-created the diagram.

--- src/ca/mcgill/cs/stg/violetta/graph/Graph.java
+++ src/ca/mcgill/cs/stg/violetta/graph/Graph.java
@@ -32,329 +32,365 @@ import java.util.Collection;
 
 import com.horstmann.violet.framework.Grid;
 
-
 /**
-   A graph consisting of selectable nodes and edges.
-*/
+ *  A graph consisting of selectable nodes and edges.
+ */
+@SuppressWarnings("serial")
 public abstract class Graph implements Serializable
 {
-	private ArrayList nodes;
-	private ArrayList edges;
-	private transient ArrayList nodesToBeRemoved;
-	private transient ArrayList edgesToBeRemoved;
-	private transient boolean needsLayout;
-	private transient Rectangle2D minBounds;
+	private ArrayList<Node> aNodes;
+	private ArrayList<Edge> aEdges;
+	private transient ArrayList<Node> aNodesToBeRemoved;
+	private transient ArrayList<Edge> aEdgesToBeRemoved;
+	private transient boolean aNeedsLayout;
+	private transient Rectangle2D aMinBounds;
 	
-   /**
-      Constructs a graph with no nodes or edges.
-   */
-   public Graph()
-   {
-      nodes = new ArrayList();
-      edges = new ArrayList();
-      nodesToBeRemoved = new ArrayList();
-      edgesToBeRemoved = new ArrayList();
-      needsLayout = true;
-   }
+	/**
+     * Constructs a graph with no nodes or edges.
+	 */
+	public Graph()
+	{
+		aNodes = new ArrayList<>();
+		aEdges = new ArrayList<>();
+		aNodesToBeRemoved = new ArrayList<>();
+		aEdgesToBeRemoved = new ArrayList<>();
+		aNeedsLayout = true;
+	}
 
-   /**
-      Adds an edge to the graph that joins the nodes containing
-      the given points. If the points aren't both inside nodes,
-      then no edge is added.
-      @param e the edge to add
-      @param p1 a point in the starting node
-      @param p2 a point in the ending node
-   */
-   public boolean connect(Edge e, Point2D p1, Point2D p2)
-   {
-      Node n1 = findNode(p1);
-      Node n2 = findNode(p2);
-      if (n1 != null)
-      {
-         e.connect(n1, n2);
-         if (n1.addEdge(e, p1, p2) && e.getEnd() != null)
-         {
-            edges.add(e);
-            if (!nodes.contains(e.getEnd()))
-               nodes.add(e.getEnd());
-            needsLayout = true;
-            return true;
-         }
-      }
-      return false;
-   }
+	/**
+     * Adds an edge to the graph that joins the nodes containing
+     * the given points. If the points aren't both inside nodes,
+     * then no edge is added.
+     * @param pEdge the edge to add
+     * @param pPoint1 a point in the starting node
+     * @param pPoint2 a point in the ending node
+     * @return true if the edge was connected
+	 */
+	public boolean connect(Edge pEdge, Point2D pPoint1, Point2D pPoint2)
+	{	
+		Node n1 = findNode(pPoint1);
+		Node n2 = findNode(pPoint2);
+		if(n1 != null)
+		{
+			pEdge.connect(n1, n2);
+			if(n1.addEdge(pEdge, pPoint1, pPoint2) && pEdge.getEnd() != null)
+			{
+				aEdges.add(pEdge);
+				if(!aNodes.contains(pEdge.getEnd()))
+				{
+					aNodes.add(pEdge.getEnd());
+				}
+				aNeedsLayout = true;
+				return true;
+			}
+		}
+		return false;
+	}
 
-   /**
-      Adds a node to the graph so that the top left corner of
-      the bounding rectangle is at the given point.
-      @param n the node to add
-      @param p the desired location
-   */
-   public boolean add(Node n, Point2D p)
-   {
-      Rectangle2D bounds = n.getBounds();
-      n.translate(p.getX() - bounds.getX(), 
-         p.getY() - bounds.getY()); 
+	/**
+     * Adds a node to the graph so that the top left corner of
+     * the bounding rectangle is at the given point.
+     * @param pNode the node to add
+     * @param pPoint the desired location
+     * @return True if the node was added.
+	 */
+	public boolean add(Node pNode, Point2D pPoint)
+	{
+		Rectangle2D bounds = pNode.getBounds();
+		pNode.translate(pPoint.getX() - bounds.getX(), pPoint.getY() - bounds.getY()); 
 
-      boolean accepted = false;
-      boolean insideANode = false;
-      for (int i = nodes.size() - 1; i >= 0 && !accepted; i--)
-      {
-         Node parent = (Node)nodes.get(i);
-         if (parent.contains(p)) 
-         {
-            insideANode = true;
-            if (parent.addNode(n, p)) accepted = true;
-         }
-      }
-      if (insideANode && !accepted) 
-         return false;
-      nodes.add(n);
-      needsLayout = true;
-      return true;
-   }
+		boolean accepted = false;
+		boolean insideANode = false;
+		for(int i = aNodes.size() - 1; i >= 0 && !accepted; i--)
+		{
+			Node parent = aNodes.get(i);
+			if (parent.contains(pPoint)) 
+			{
+				insideANode = true;
+				if (parent.addNode(pNode, pPoint))
+				{
+					accepted = true;
+				}
+			}	
+			}
+		if(insideANode && !accepted)
+		{
+			return false;
+		}
+		aNodes.add(pNode);
+		aNeedsLayout = true;
+		return true;
+	}
 
-   /**
+	/**
       Finds a node containing the given point.
-      @param p a point
+      @param pPoint a point
       @return a node containing p or null if no nodes contain p
-   */
-   public Node findNode(Point2D p)
-   {
-      for (int i = nodes.size() - 1; i >= 0; i--)
-      {
-         Node n = (Node)nodes.get(i);
-         if (n.contains(p)) return n;
-      }
-      return null;
-   }
+	 */
+	public Node findNode(Point2D pPoint)
+	{
+		for(int i = aNodes.size() - 1; i >= 0; i--)
+		{
+			Node n = aNodes.get(i);
+			if(n.contains(pPoint))
+			{
+				return n;
+			}
+		}
+		return null;
+	}
 
-   /**
-      Finds an edge containing the given point.
-      @param p a point
-      @return an edge containing p or null if no edges contain p
-   */
-   public Edge findEdge(Point2D p)
-   {
-      for (int i = edges.size() - 1; i >= 0; i--)
-      {
-         Edge e = (Edge)edges.get(i);
-         if (e.contains(p)) return e;
-      }
-      return null;
-   }
+	/**
+     * Finds an edge containing the given point.
+     * @param pPoint a point
+     * @return an edge containing p or null if no edges contain p
+	 */
+	public Edge findEdge(Point2D pPoint)
+	{
+		for (int i = aEdges.size() - 1; i >= 0; i--)
+		{
+			Edge e = aEdges.get(i);
+			if(e.contains(pPoint))
+			{
+				return e;
+			}
+		}
+		return null;
+	}
    
-   /**
-      Draws the graph
-      @param g2 the graphics context
-   */
-   public void draw(Graphics2D g2, Grid g)
-   {
-      layout(g2, g);
+	/**
+     * Draws the graph.
+     * @param pGraphics2D the graphics context
+     * @param pGrid The grid
+	 */
+	public void draw(Graphics2D pGraphics2D, Grid pGrid)
+	{
+		layout(pGraphics2D, pGrid);
 
-      for (int i = 0; i < nodes.size(); i++)
-      {
-         Node n = (Node)nodes.get(i);
-         n.draw(g2);
-      }
+		for(int i = 0; i < aNodes.size(); i++)
+		{
+			Node n = aNodes.get(i);
+			n.draw(pGraphics2D);
+		}
 
-      for (int i = 0; i < edges.size(); i++)
-      {
-         Edge e = (Edge)edges.get(i);
-         e.draw(g2);
-      }
-   }
+		for(int i = 0; i < aEdges.size(); i++)
+		{
+			Edge e = aEdges.get(i);
+			e.draw(pGraphics2D);
+		}
+	}
    
-   /**
-      Removes a node and all edges that start or end with that node
-      @param n the node to remove
-   */
-   public void removeNode(Node n)
-   {
-      if (nodesToBeRemoved.contains(n)) return;
-      nodesToBeRemoved.add(n);
-      // notify nodes of removals
-      for (int i = 0; i < nodes.size(); i++)
-      {
-         Node n2 = (Node)nodes.get(i);
-         n2.removeNode(this, n);
-      }
-      for (int i = 0; i < edges.size(); i++)
-      {
-         Edge e = (Edge)edges.get(i);
-         if (e.getStart() == n || e.getEnd() == n)
-            removeEdge(e);
-      }
+	/**
+     * Removes a node and all edges that start or end with that node.
+     * @param pNode the node to remove
+	 */
+	public void removeNode(Node pNode)
+	{
+		if(aNodesToBeRemoved.contains(pNode))
+		{
+			return;
+		}
+		aNodesToBeRemoved.add(pNode);
+      
+		// notify nodes of removals
+		for(int i = 0; i < aNodes.size(); i++)
+		{
+			Node n2 = aNodes.get(i);
+			n2.removeNode(this, pNode);
+		}
+		for(int i = 0; i < aEdges.size(); i++)
+		{
+			Edge e = aEdges.get(i);
+			if(e.getStart() == pNode || e.getEnd() == pNode)
+			{
+				removeEdge(e);
+			}
+		}
+		aNeedsLayout = true;
+	}
 
-      needsLayout = true;
-   }
+	/**
+     * Removes an edge from the graph.
+     * @param pEdge the edge to remove
+	 */
+	public void removeEdge(Edge pEdge)
+	{
+		if (aEdgesToBeRemoved.contains(pEdge))
+		{
+			return;
+		}
+		aEdgesToBeRemoved.add(pEdge);
+		for(int i = aNodes.size() - 1; i >= 0; i--)
+		{
+			Node n = aNodes.get(i);
+			n.removeEdge(this, pEdge);
+		}
+		aNeedsLayout = true;
+	}
 
-   /**
-      Removes an edge from the graph.
-      @param e the edge to remove
-   */
-   public void removeEdge(Edge e)
-   {
-      if (edgesToBeRemoved.contains(e)) return;
-      edgesToBeRemoved.add(e);
-      for (int i = nodes.size() - 1; i >= 0; i--)
-      {
-         Node n = (Node)nodes.get(i);
-         n.removeEdge(this, e);
-      }
-      needsLayout = true;
-   }
+	/**
+     * Causes the layout of the graph to be recomputed.
+	 */
+	public void layout()
+	{
+		aNeedsLayout = true;
+	}
 
-   /**
-      Causes the layout of the graph to be recomputed.
-   */
-   public void layout()
-   {
-      needsLayout = true;
-   }
+  	/**
+     * Computes the layout of the graph.
+     * If you override this method, you must first call 
+     * <code>super.layout</code>.
+     * @param pGraphics2D the graphics context
+     * @param pGrid the grid to snap to
+  	 */
+	protected void layout(Graphics2D pGraphics2D, Grid pGrid)
+	{
+		if(!aNeedsLayout)
+		{
+			return;
+		}
+		aNodes.removeAll(aNodesToBeRemoved);
+		aEdges.removeAll(aEdgesToBeRemoved);
+		aNodesToBeRemoved.clear();
+		aEdgesToBeRemoved.clear();
 
-   /**
-      Computes the layout of the graph.
-      If you override this method, you must first call 
-      <code>super.layout</code>.
-      @param g2 the graphics context
-      @param g the grid to snap to
-   */
-   protected void layout(Graphics2D g2, Grid g)
-   {
-      if (!needsLayout) return;
-      nodes.removeAll(nodesToBeRemoved);
-      edges.removeAll(edgesToBeRemoved);
-      nodesToBeRemoved.clear();
-      edgesToBeRemoved.clear();
+		for(int i = 0; i < aNodes.size(); i++)
+		{
+			Node n = aNodes.get(i);
+			n.layout(this, pGraphics2D, pGrid);
+		}
+		aNeedsLayout = false;
+	}
 
-      for (int i = 0; i < nodes.size(); i++)
-      {
-         Node n = (Node) nodes.get(i);
-         n.layout(this, g2, g);
-      }
-      needsLayout = false;
-   }
-
-   /**
-      Gets the smallest rectangle enclosing the graph
-      @param g2 the graphics context
-      @return the bounding rectangle
-   */
-   public Rectangle2D getBounds(Graphics2D g2)
-   {
-      Rectangle2D r = minBounds;
-      for (int i = 0; i < nodes.size(); i++)
-      {
-         Node n = (Node)nodes.get(i);
-         Rectangle2D b = n.getBounds();
-         if (r == null) r = b;
-         else r.add(b);
-      }
-      for (int i = 0; i < edges.size(); i++)
-      {
-         Edge e = (Edge)edges.get(i);
-         r.add(e.getBounds(g2));
-      }
-      return r == null ? new Rectangle2D.Double() : new Rectangle2D.Double(r.getX(), r.getY(), 
-            r.getWidth() + AbstractNode.SHADOW_GAP, r.getHeight() + AbstractNode.SHADOW_GAP);
-   }
+	/**
+     * Gets the smallest rectangle enclosing the graph.
+     * @param pGraphics2D the graphics context
+     * @return the bounding rectangle
+	 */
+	public Rectangle2D getBounds(Graphics2D pGraphics2D)
+	{
+		Rectangle2D r = aMinBounds;
+		for(int i = 0; i < aNodes.size(); i++)
+		{
+			Node n = aNodes.get(i);
+			Rectangle2D b = n.getBounds();
+			if(r == null)
+			{
+				r = b;
+			}
+			else
+			{
+				r.add(b);
+			}
+		}
+		for(int i = 0; i < aEdges.size(); i++)
+		{
+			Edge e = aEdges.get(i);
+			r.add(e.getBounds(pGraphics2D));
+		}
+		if(r == null )
+		{
+			return new Rectangle2D.Double();
+		}
+		else
+		{
+			return new Rectangle2D.Double(r.getX(), r.getY(), r.getWidth() + AbstractNode.SHADOW_GAP, r.getHeight() + AbstractNode.SHADOW_GAP);
+		}
+	}
    
-   public Rectangle2D getMinBounds() { return minBounds; }
-   public void setMinBounds(Rectangle2D newValue) { minBounds = newValue; }
+	/**
+	 * @return The minimum bounds property
+	 */
+	public Rectangle2D getMinBounds() 
+	{ return aMinBounds; }
+   
+	/**
+	 * @param pMinBounds The minimum bounds property
+	 */
+	public void setMinBounds(Rectangle2D pMinBounds)
+	{ aMinBounds = pMinBounds; }
 
-   /**
-      Gets the node types of a particular graph type.
-      @return an array of node prototypes
-   */   
-   public abstract Node[] getNodePrototypes();
+	/**
+     * Gets the node types of a particular graph type.
+     * @return an array of node prototypes
+	 */   
+	public abstract Node[] getNodePrototypes();
 
-   /**
-      Gets the edge types of a particular graph type.
-      @return an array of edge prototypes
-   */   
-   public abstract Edge[] getEdgePrototypes();
+	/**
+     * Gets the edge types of a particular graph type.
+     * @return an array of edge prototypes
+	 */   
+	public abstract Edge[] getEdgePrototypes();
  
-   /**
-      Adds a persistence delegate to a given encoder that
-      encodes the child nodes of this node.
-      @param encoder the encoder to which to add the delegate
-   */
-   public static void setPersistenceDelegate(Encoder encoder)
-   {
-      encoder.setPersistenceDelegate(Graph.class, new
-         DefaultPersistenceDelegate()
-         {
-            protected void initialize(Class type, 
-               Object oldInstance, Object newInstance, 
-               Encoder out) 
-            {
-               super.initialize(type, oldInstance, 
-                  newInstance, out);
-               Graph g = (Graph)oldInstance;
+	/**
+     * Adds a persistence delegate to a given encoder that
+     * encodes the child nodes of this node.
+     * @param pEncoder the encoder to which to add the delegate
+	 */
+	public static void setPersistenceDelegate(Encoder pEncoder)
+	{
+		pEncoder.setPersistenceDelegate(Graph.class, new DefaultPersistenceDelegate()
+			{
+            	protected void initialize(Class<?> pType, Object pOldInstance, Object pNewInstance, Encoder pOut) 
+            	{
+            		super.initialize(pType, pOldInstance, pNewInstance, pOut);
+            		Graph g = (Graph)pOldInstance;
          
-               for (int i = 0; i < g.nodes.size(); i++)
-               {
-                  Node n = (Node)g.nodes.get(i);
-                  Rectangle2D bounds = n.getBounds();
-                  Point2D p = new Point2D.Double(bounds.getX(),
-                     bounds.getY());
-                  out.writeStatement(
-                     new Statement(oldInstance,
-                        "addNode", new Object[]{ n, p }) );
-               }
-               for (int i = 0; i < g.edges.size(); i++)
-               {
-                  Edge e = (Edge)g.edges.get(i);
-                  out.writeStatement(
-                     new Statement(oldInstance,
-                        "connect", 
-                        new Object[]{ e, e.getStart(), e.getEnd() }) );            
-               }
-            }
-         });
-   }
+            		for(int i = 0; i < g.aNodes.size(); i++)
+            		{
+            			Node n = g.aNodes.get(i);
+            			Rectangle2D bounds = n.getBounds();
+            			Point2D p = new Point2D.Double(bounds.getX(), bounds.getY());
+            			pOut.writeStatement( new Statement(pOldInstance, "addNode", new Object[]{ n, p }) );
+            		}
+            		for(int i = 0; i < g.aEdges.size(); i++)
+            		{
+            			Edge e = g.aEdges.get(i);
+            			pOut.writeStatement( new Statement(pOldInstance, "connect", new Object[]{ e, e.getStart(), e.getEnd() }) );            
+            		}
+            	}
+			});
+	}
 
-   /**
-      Gets the nodes of this graph.
-      @return an unmodifiable collection of the nodes
-   */
-   public Collection getNodes() { return nodes; }
+	/**
+     * Gets the nodes of this graph.
+     * @return an unmodifiable collection of the nodes
+	 */
+	public Collection<Node> getNodes()
+	{ return aNodes; }
 
-   /**
-      Gets the edges of this graph.
-      @return an unmodifiable collection of the edges
-   */
-   public Collection getEdges() { return edges; }
+	/**
+     * Gets the edges of this graph.
+     * @return an unmodifiable collection of the edges
+	 */
+	public Collection<Edge> getEdges() 
+	{ return aEdges; }
 
-   /**
-      Adds a node to this graph. This method should
-      only be called by a decoder when reading a data file.
-      @param n the node to add
-      @param p the desired location
-   */
-   public void addNode(Node n, Point2D p)
-   {
-      Rectangle2D bounds = n.getBounds();
-      n.translate(p.getX() - bounds.getX(), 
-         p.getY() - bounds.getY()); 
-      nodes.add(n); 
-   }
+	/**
+     * Adds a node to this graph. This method should
+     * only be called by a decoder when reading a data file.
+     * @param pNode the node to add
+     * @param pPoint the desired location
+	 */
+	public void addNode(Node pNode, Point2D pPoint)
+	{
+		Rectangle2D bounds = pNode.getBounds();
+		pNode.translate(pPoint.getX() - bounds.getX(), pPoint.getY() - bounds.getY()); 
+		aNodes.add(pNode); 
+	}
 
-   /**
-      Adds an edge to this graph. This method should
-      only be called by a decoder when reading a data file.
-      @param e the edge to add
-      @param start the start node of the edge
-      @param end the end node of the edge
-   */
-   public void connect(Edge e, Node start, Node end)
-   {
-      e.connect(start, end);
-      edges.add(e);
-   }
-
-   
+	/**
+     * Adds an edge to this graph. This method should
+     * only be called by a decoder when reading a data file.
+     * @param pEdge the edge to add
+     * @param pStart the start node of the edge
+     * @param pEnd the end node of the edge
+	 */
+	public void connect(Edge pEdge, Node pStart, Node pEnd)
+	{
+     	pEdge.connect(pStart, pEnd);
+     	aEdges.add(pEdge);
+	}
 }
 
 
