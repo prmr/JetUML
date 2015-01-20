c8f06032abbe0e26eb94c7a9dd4c15b54a2a58f2
Eric Quinn <EJBQuinn@gmail.com>
2015-01-20 14:15:46 -0500

    Changed the "open" method in EditorFrame to check if the name of a frame is null before comparing it to the string to open.

--- src/com/horstmann/violet/framework/EditorFrame.java
+++ src/com/horstmann/violet/framework/EditorFrame.java
@@ -664,7 +664,7 @@ public class EditorFrame extends JFrame
          if (frames[i] instanceof GraphFrame)
          {
             GraphFrame frame = (GraphFrame)frames[i];
-            if (frame.getFileName().equals(name)) 
+            if (frame.getFileName() != null && frame.getFileName().equals(name)) 
             {
                try
                {
