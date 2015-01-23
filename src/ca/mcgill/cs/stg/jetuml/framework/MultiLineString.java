7eefa8dabfa38ce62d38d3eeebdb34fc6ae6f09f
prmr <prmr@gmx.com>
2015-01-22 20:30:18 -0500

    #27 Code cleanups

--- src/ca/mcgill/cs/stg/jetuml/framework/MultiLineString.java
+++ src/ca/mcgill/cs/stg/jetuml/framework/MultiLineString.java
@@ -32,184 +32,239 @@ import java.util.StringTokenizer;
 import javax.swing.JLabel;
 
 /**
-   A string that can extend over multiple lines.
-*/
+ *   A string that can extend over multiple lines.
+ */
 public class MultiLineString implements Cloneable
 {
-	   public static final int LEFT = 0;
-	   public static final int CENTER = 1;
-	   public static final int RIGHT = 2;
-	   public static final int LARGE = 3;
-	   public static final int NORMAL = 4;
-	   public static final int SMALL = 5;
+	public static final int LEFT = 0;
+	public static final int CENTER = 1;
+	public static final int RIGHT = 2;
+	public static final int LARGE = 3;
+	public static final int NORMAL = 4;
+	public static final int SMALL = 5;
 
-	   private static final int GAP = 3;
-
-	   private String text;
-	   private int justification;
-	   private int size;
-	   private boolean underlined;
-	   
-	   private transient JLabel label = new JLabel();
+	private String aText;
+	private int aJustification;
+	private int aSize;
+	private boolean aUnderlined;
+	private JLabel aLabel = new JLabel();
 	
-   /**
-      Constructs an empty, centered, normal size multiline
-      string that is not underlined.
-   */
-   public MultiLineString() 
-   { 
-      text = ""; 
-      justification = CENTER;
-      size = NORMAL;
-      underlined = false;    
-   }
-   /**
-      Sets the value of the text property.
-      @param newValue the text of the multiline string
-   */
-   public void setText(String newValue) { text = newValue; setLabelText(); }
-   /**
-      Gets the value of the text property.
-      @return the text of the multiline string
-   */
-   public String getText() { return text; }
-   /**
-      Sets the value of the justification property.
-      @param newValue the justification, one of LEFT, CENTER, 
-      RIGHT
-   */
-   public void setJustification(int newValue) { justification = newValue; setLabelText(); }
-   /**
-      Gets the value of the justification property.
-      @return the justification, one of LEFT, CENTER, 
-      RIGHT
-   */
-   public int getJustification() { return justification; }
-   /**
-      Gets the value of the underlined property.
-      @return true if the text is underlined
-   */
-   public boolean isUnderlined() { return underlined; }
-   /**
-      Sets the value of the underlined property.
-      @param newValue true to underline the text
-   */
-   public void setUnderlined(boolean newValue) { underlined = newValue; setLabelText(); }
-   /**
-      Sets the value of the size property.
-      @param newValue the size, one of SMALL, NORMAL, LARGE
-   */
-   public void setSize(int newValue) { size = newValue; setLabelText(); }
-   /**
-      Gets the value of the size property.
-      @return the size, one of SMALL, NORMAL, LARGE
-   */
-   public int getSize() { return size; }
+	/**
+     * Constructs an empty, centered, normal size multiline
+     * string that is not underlined.
+	 */
+	public MultiLineString() 
+	{ 
+		aText = ""; 
+		aJustification = CENTER;
+		aSize = NORMAL;
+		aUnderlined = false;    
+	}
+	
+	/**
+     * Sets the value of the text property.
+     * @param pText the text of the multiline string
+	 */
+	public void setText(String pText)
+	{ 
+		aText = pText; 
+		setLabelText(); 
+	}
    
-   public String toString()
-   {
-      return text.replace('\n', '|');
-   }
+	/**
+     * Gets the value of the text property.
+     * @return the text of the multiline string
+	 */
+	public String getText() 
+	{ 
+		return aText; 
+	}
+   
+	/**
+     * Sets the value of the justification property.
+     * @param pJustification the justification, one of LEFT, CENTER, RIGHT
+	 */
+	public void setJustification(int pJustification) 
+	{ 
+		aJustification = pJustification; 
+		setLabelText(); 
+	}
+   
+	/**
+     * Gets the value of the justification property.
+     * @return the justification, one of LEFT, CENTER, RIGHT
+	 */
+	public int getJustification() 
+	{ 
+		return aJustification;
+	}
+   
+	/**
+     * Gets the value of the underlined property.
+     * @return true if the text is underlined
+	 */
+	public boolean isUnderlined() 
+	{ 
+		return aUnderlined;
+	}
+  
+	/**
+     * Sets the value of the underlined property.
+     * @param pUnderlined true to underline the text
+	 */
+	public void setUnderlined(boolean pUnderlined) 
+	{ 
+		aUnderlined = pUnderlined; 
+		setLabelText(); 
+	}
+   
+	/**
+     * Sets the value of the size property.
+     * @param pSize the size, one of SMALL, NORMAL, LARGE
+	 */
+	public void setSize(int pSize) 
+	{ 
+		aSize = pSize; 
+		setLabelText(); 
+	}
+  
+	/**
+     * Gets the value of the size property.
+     * @return the size, one of SMALL, NORMAL, LARGE
+	 */
+	public int getSize() 
+	{ 
+		return aSize; 
+	}
+   
+	@Override
+	public String toString()
+	{
+		return aText.replace('\n', '|');
+	}
 
-   private void setLabelText()
-   {
-      StringBuffer prefix = new StringBuffer();
-      StringBuffer suffix = new StringBuffer();
-      StringBuffer htmlText = new StringBuffer();
-      prefix.append("&nbsp;");
-      suffix.insert(0, "&nbsp;");
-      if (underlined) 
-      {
-         prefix.append("<u>");
-         suffix.insert(0, "</u>");
-      }
-      if (size == LARGE)
-      {
-         prefix.append("<font size=\"+1\">");
-         suffix.insert(0, "</font>");
-      }
-      if (size == SMALL)
-      {
-         prefix.append("<font size=\"-1\">");
-         suffix.insert(0, "</font>");
-      }
-      htmlText.append("<html>");
-      StringTokenizer tokenizer = new StringTokenizer(text, "\n");
-      boolean first = true;
-      while (tokenizer.hasMoreTokens())
-      {
-         if (first) first = false; else htmlText.append("<br>");
-         htmlText.append(prefix);
-         htmlText.append(tokenizer.nextToken());
-         htmlText.append(suffix);
-      }      
-      htmlText.append("</html>");
+	private void setLabelText()
+	{
+		StringBuffer prefix = new StringBuffer();
+		StringBuffer suffix = new StringBuffer();
+		StringBuffer htmlText = new StringBuffer();
+		prefix.append("&nbsp;");
+		suffix.insert(0, "&nbsp;");
+		if(aUnderlined) 
+		{
+			prefix.append("<u>");
+			suffix.insert(0, "</u>");
+		}
+		if(aSize == LARGE)
+		{
+			prefix.append("<font size=\"+1\">");
+			suffix.insert(0, "</font>");
+		}
+		if(aSize == SMALL)
+		{
+			prefix.append("<font size=\"-1\">");
+			suffix.insert(0, "</font>");
+		}
+		htmlText.append("<html>");
+		StringTokenizer tokenizer = new StringTokenizer(aText, "\n");
+		boolean first = true;
+		while (tokenizer.hasMoreTokens())
+		{
+			if(first) 
+			{
+				first = false;
+			}
+			else 
+			{
+				htmlText.append("<br>");
+			}
+			htmlText.append(prefix);
+			htmlText.append(tokenizer.nextToken());
+			htmlText.append(suffix);
+		}      
+		htmlText.append("</html>");
       
-      // replace any < that are not followed by {u, i, b, tt, font, br} with &lt;
+		// replace any < that are not followed by {u, i, b, tt, font, br} with &lt;
       
-      List dontReplace = Arrays.asList(new String[] { "u", "i", "b", "tt", "font", "br" });
+		List<String> dontReplace = Arrays.asList(new String[] { "u", "i", "b", "tt", "font", "br" });
       
-      int ltpos = 0;
-      while (ltpos != -1)
-      {
-         ltpos = htmlText.indexOf("<", ltpos + 1);
-         if (ltpos != -1 && !(ltpos + 1 < htmlText.length() && htmlText.charAt(ltpos + 1) == '/'))
-         {
-            int end = ltpos + 1;
-            while (end < htmlText.length() && Character.isLetter(htmlText.charAt(end))) end++;
-            if (!dontReplace.contains(htmlText.substring(ltpos + 1, end)))
-               htmlText.replace(ltpos, ltpos+1, "&lt;");
-         }
-      }
+		int ltpos = 0;
+		while(ltpos != -1)
+		{	
+			ltpos = htmlText.indexOf("<", ltpos + 1);
+			if(ltpos != -1 && !(ltpos + 1 < htmlText.length() && htmlText.charAt(ltpos + 1) == '/'))
+			{
+				int end = ltpos + 1;
+				while(end < htmlText.length() && Character.isLetter(htmlText.charAt(end))) 
+				{
+					end++;
+				}
+				if(!dontReplace.contains(htmlText.substring(ltpos + 1, end)))
+				{
+					htmlText.replace(ltpos, ltpos+1, "&lt;");
+				}
+			}
+		}
             
-      label.setText(htmlText.toString());
-      if (justification == LEFT) label.setHorizontalAlignment(JLabel.LEFT);
-      else if (justification == CENTER) label.setHorizontalAlignment(JLabel.CENTER);
-      else if (justification == RIGHT) label.setHorizontalAlignment(JLabel.RIGHT);
-   }
+		aLabel.setText(htmlText.toString());
+		if(aJustification == LEFT)
+		{
+			aLabel.setHorizontalAlignment(JLabel.LEFT);
+		}
+		else if(aJustification == CENTER)
+		{
+			aLabel.setHorizontalAlignment(JLabel.CENTER);
+		}
+		else if(aJustification == RIGHT) 
+		{
+			aLabel.setHorizontalAlignment(JLabel.RIGHT);
+		}
+	}
    
-   /**
-      Gets the bounding rectangle for this multiline string.
-      @param g2 the graphics context
-      @return the bounding rectangle (with top left corner (0,0))
-   */
-   public Rectangle2D getBounds(Graphics2D g2)
-   {
-      if (text.length() == 0) return new Rectangle2D.Double();
-      // setLabelText();
-      Dimension dim = label.getPreferredSize();       
-      return new Rectangle2D.Double(0, 0, dim.getWidth(), dim.getHeight());
-   }
+	/**
+     * Gets the bounding rectangle for this multiline string.
+     * @param pGraphics2D the graphics context
+     * @return the bounding rectangle (with top left corner (0,0))
+	 */
+	public Rectangle2D getBounds(Graphics2D pGraphics2D)
+	{
+		if(aText.length() == 0) 
+		{
+			return new Rectangle2D.Double();
+		}
+		// setLabelText();
+		Dimension dim = aLabel.getPreferredSize();       
+		return new Rectangle2D.Double(0, 0, dim.getWidth(), dim.getHeight());
+	}
 
-   /**
-      Draws this multiline string inside a given rectangle
-      @param g2 the graphics context
-      @param r the rectangle into which to place this multiline string
-   */
-   public void draw(Graphics2D g2, Rectangle2D r)
-   {
-      // setLabelText();
-      label.setFont(g2.getFont());
-      label.setBounds(0, 0, (int) r.getWidth(), (int) r.getHeight());
-      g2.translate(r.getX(), r.getY());
-      label.paint(g2);
-      g2.translate(-r.getX(), -r.getY());        
-   }
-
-   public Object clone()
-   {
-      try
-      {
-         MultiLineString cloned = (MultiLineString) super.clone();
-         cloned.label = new JLabel();
-         cloned.setLabelText();
-         return cloned;
-      }
-      catch (CloneNotSupportedException exception)
-      {
-         return null;
-      }
-   }
-
-
+	/**
+     * Draws this multiline string inside a given rectangle.
+     * @param pGraphics2D the graphics context
+     * @param pRectangle the rectangle into which to place this multiline string
+	 */
+	public void draw(Graphics2D pGraphics2D, Rectangle2D pRectangle)
+	{
+		// setLabelText();
+		aLabel.setFont(pGraphics2D.getFont());
+		aLabel.setBounds(0, 0, (int) pRectangle.getWidth(), (int) pRectangle.getHeight());
+		pGraphics2D.translate(pRectangle.getX(), pRectangle.getY());
+		aLabel.paint(pGraphics2D);
+		pGraphics2D.translate(-pRectangle.getX(), -pRectangle.getY());        
+	}
+	
+	@Override
+	public Object clone()
+	{
+		try
+		{
+			MultiLineString cloned = (MultiLineString) super.clone();
+			cloned.aLabel = new JLabel();
+			cloned.setLabelText();
+			return cloned;
+		}
+		catch (CloneNotSupportedException exception)
+		{
+			return null;
+		}
+	}
 }
