25acae371d8e00817b8a0a2b7680dadad19d60ae
EJBQ <EJBQuinn@gmail.com>
2015-02-06 08:00:04 -0500

    #13 Partially fixed undoing of deleting edges and edges in groups

--- src/ca/mcgill/cs/stg/jetuml/framework/AddDeleteEdgeCommand.java
+++ src/ca/mcgill/cs/stg/jetuml/framework/AddDeleteEdgeCommand.java
@@ -5,21 +5,21 @@ import java.awt.geom.Point2D;
 
 import ca.mcgill.cs.stg.jetuml.graph.Edge;
 import ca.mcgill.cs.stg.jetuml.graph.Graph;
+import ca.mcgill.cs.stg.jetuml.graph.Node;
 
 public class AddDeleteEdgeCommand implements Command{
 	Edge aEdge;
 	Graph aGraph;
-	Point2D aP1;
-	Point2D aP2;
+	Node aP1;
+	Node aP2;
 	boolean aAdding; //true for adding, false for deleting
 	
 	public AddDeleteEdgeCommand(Graph pGraph, Edge pEdge, boolean pAdding)
 	{
 		aGraph = pGraph;
 		aEdge = pEdge;
-		Line2D ends = aEdge.getConnectionPoints();
-		aP1 = ends.getP1();
-		aP2 = ends.getP2();
+		aP1 = aEdge.getStart();
+		aP2 = aEdge.getEnd();
 		aAdding = pAdding;
 	}
 	
@@ -58,7 +58,7 @@ public class AddDeleteEdgeCommand implements Command{
 	
 	private void add() 
 	{
-		aGraph.connect(aEdge, aGraph.findNode(aP1), aGraph.findNode(aP2));
+		aGraph.connect(aEdge, aP1, aP2);
 		aGraph.layout();
 	}
 	
