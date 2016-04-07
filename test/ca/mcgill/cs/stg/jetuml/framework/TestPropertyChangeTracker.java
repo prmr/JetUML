64610a6ce3c1911fed5d2d72d254f4598c3bcb0c
prmr <prmr@gmx.com>
2016-04-07 07:20:39 -0400

    #154 Tests for PropertyChangeTracker

--- test/ca/mcgill/cs/stg/jetuml/framework/TestPropertyChangeTracker.java
+++ test/ca/mcgill/cs/stg/jetuml/framework/TestPropertyChangeTracker.java
@@ -18,7 +18,10 @@ import ca.mcgill.cs.stg.jetuml.commands.Command;
 import ca.mcgill.cs.stg.jetuml.commands.CompoundCommand;
 import ca.mcgill.cs.stg.jetuml.commands.PropertyChangeCommand;
 import ca.mcgill.cs.stg.jetuml.diagrams.ClassDiagramGraph;
+import ca.mcgill.cs.stg.jetuml.diagrams.ObjectDiagramGraph;
+import ca.mcgill.cs.stg.jetuml.graph.AggregationEdge;
 import ca.mcgill.cs.stg.jetuml.graph.ClassNode;
+import ca.mcgill.cs.stg.jetuml.graph.FieldNode;
 
 /**
  * Note that the logic of the tests in this class relies
@@ -75,7 +78,77 @@ public class TestPropertyChangeTracker
 		assertEquals(oldAttributes, getFieldValue(aOldValueField, pcc));
 		assertEquals(node.getAttributes(), getFieldValue(aNewValueField, pcc));
 	}
+	
+	@Test
+	public void testObjectNode()
+	{
+		FieldNode node = new FieldNode();
+		MultiLineString oldName = node.getName().clone();
+		MultiLineString oldValue = node.getValue();
+		oldValue.setText("value");
+		oldValue = oldValue.clone();
+	
+		aTracker.startTrackingPropertyChange(node);
+		node.getName().setText("Foo");
+		node.setBoxedValue(true);
+		node.setValue(new MultiLineString());
+		
+		CompoundCommand command = aTracker.stopTrackingPropertyChange(new ObjectDiagramGraph());
+		Stack<Command> commands = getChildCommands(command);
+		assertEquals(3, commands.size());
+		
+		PropertyChangeCommand pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("value", getPropertyName(pcc, node));
+		assertEquals(node, getFieldValue(aObjectField, pcc));
+		assertEquals(oldValue, getFieldValue(aOldValueField, pcc));
+		assertEquals(node.getValue(), getFieldValue(aNewValueField, pcc));
+		
+		pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("name", getPropertyName(pcc, node));
+		assertEquals(node, getFieldValue(aObjectField, pcc));
+		assertEquals(oldName, getFieldValue(aOldValueField, pcc));
+		assertEquals(node.getName(), getFieldValue(aNewValueField, pcc));
+		
+		pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("boxedValue", getPropertyName(pcc, node));
+		assertEquals(node, getFieldValue(aObjectField, pcc));
+		assertEquals(false, getFieldValue(aOldValueField, pcc));
+		assertEquals(true, getFieldValue(aNewValueField, pcc));
+	}
+	
+	@Test
+	public void testAggregationEdge()
+	{
+		AggregationEdge edge = new AggregationEdge();
+	
+		aTracker.startTrackingPropertyChange(edge);
+		edge.setStartLabel("start");
+		edge.setEndLabel("end");
+		edge.setType(AggregationEdge.Type.Composition);
 
+		CompoundCommand command = aTracker.stopTrackingPropertyChange(new ClassDiagramGraph());
+		Stack<Command> commands = getChildCommands(command);
+		assertEquals(3, commands.size());
+		
+		PropertyChangeCommand pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("type", getPropertyName(pcc, edge));
+		assertEquals(edge, getFieldValue(aObjectField, pcc));
+		assertEquals(AggregationEdge.Type.Aggregation, getFieldValue(aOldValueField, pcc));
+		assertEquals(AggregationEdge.Type.Composition, getFieldValue(aNewValueField, pcc));
+		
+		pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("startLabel", getPropertyName(pcc, edge));
+		assertEquals(edge, getFieldValue(aObjectField, pcc));
+		assertEquals("", getFieldValue(aOldValueField, pcc));
+		assertEquals("start", getFieldValue(aNewValueField, pcc));
+		
+		pcc = (PropertyChangeCommand)commands.pop();
+		assertEquals("endLabel", getPropertyName(pcc, edge));
+		assertEquals(edge, getFieldValue(aObjectField, pcc));
+		assertEquals("", getFieldValue(aOldValueField, pcc));
+		assertEquals("end", getFieldValue(aNewValueField, pcc));
+	}
+	
 	@SuppressWarnings("unchecked")
 	private Stack<Command> getChildCommands(CompoundCommand pCommand)
 	{
