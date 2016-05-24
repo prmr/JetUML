a5b04e3391155bd47969c9a5f43d4194cbaaca0e
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-24 15:48:13 -0400

    Add TestAddEdgeCommand

--- test/ca/mcgill/cs/stg/jetuml/commands/TestAddEdgeCommandTest.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestAddEdgeCommandTest.java
@@ -2,7 +2,6 @@ package ca.mcgill.cs.stg.jetuml.commands;
 
 import static org.junit.Assert.*;
 
-import java.awt.geom.Point2D;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
@@ -10,37 +9,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ca.mcgill.cs.stg.jetuml.diagrams.ClassDiagramGraph;
-import ca.mcgill.cs.stg.jetuml.graph.ClassNode;
+import ca.mcgill.cs.stg.jetuml.graph.CallEdge;
+import ca.mcgill.cs.stg.jetuml.graph.Edge;
 import ca.mcgill.cs.stg.jetuml.graph.Graph;
-import ca.mcgill.cs.stg.jetuml.graph.Node;
 
-public class TestAddNodeCommand {
+public class TestAddEdgeCommandTest {
     private Graph aGraph;
     private Field aNeedsLayout;
-    private Field aNodesToBeRemoved;
-    private Node aNode;
-    private ArrayList<Node> aRootNodes;
-    private AddNodeCommand aAddNodeCommand;
+    private Field aEdgesToBeRemoved;
+    Edge aEdge;
+    private ArrayList<Edge> aEdges;
+    private AddEdgeCommand aAddEdgeCommand;
     private boolean hasExecuted = false;
 
     @Before
     public void setUp() throws Exception {
         aGraph = new ClassDiagramGraph();
-        aRootNodes = (ArrayList<Node>) aGraph.getRootNodes();
-        aNode = new ClassNode();
+        aEdges = (ArrayList<Edge>) aGraph.getEdges();
         aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
         aNeedsLayout.setAccessible(true);
-        aNodesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aNodesToBeRemoved");
-        aNodesToBeRemoved.setAccessible(true);
-        aAddNodeCommand = new AddNodeCommand(aGraph, aNode);  
+        aEdgesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aEdgesToBeRemoved");
+        aEdgesToBeRemoved.setAccessible(true);
+        aEdge = new CallEdge();
+        aAddEdgeCommand = new AddEdgeCommand(aGraph, aEdge);  
     }
 
     @Test
     public void testExecute() {
-        int numOfNodes = aRootNodes.size();
-        aAddNodeCommand.execute();
+        aAddEdgeCommand.execute();
         hasExecuted = true;
-        assertEquals(numOfNodes+1, aRootNodes.size());
+        assertTrue(aEdges.contains(aEdge));
         try {
             assertTrue((boolean)aNeedsLayout.get(aGraph));
         } catch (IllegalArgumentException e) {
@@ -51,19 +49,19 @@ public class TestAddNodeCommand {
     }
     
     @Test
-    public void testUndo() {
+    public void testUndo(){
         if (!hasExecuted)
         {
-            aAddNodeCommand.execute();
+            aAddEdgeCommand.execute();
         }
-        aAddNodeCommand.undo();
+        aAddEdgeCommand.undo();
         try {
-            ArrayList<Node> aListNodesToBeRemoved = (ArrayList<Node>)(aNodesToBeRemoved.get(aGraph));
-            assertTrue(aListNodesToBeRemoved.contains((Node)aNode));
-        } catch (IllegalArgumentException e1) {
-            e1.printStackTrace();
-        } catch (IllegalAccessException e1) {
-            e1.printStackTrace();
+            ArrayList<Edge> aListEdgesToBeRemoved = (ArrayList<Edge>) aEdgesToBeRemoved.get(aGraph);
+            assertTrue(aListEdgesToBeRemoved.contains(aEdge));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
         try {
             assertTrue((boolean)aNeedsLayout.get(aGraph));
@@ -71,7 +69,6 @@ public class TestAddNodeCommand {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-        } 
+        }
     }
-
 }
