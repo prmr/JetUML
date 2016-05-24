6a146b0cca76863a22b115cfb1fff2a38c3b910a
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-24 15:58:05 -0400

    update changes

--- test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
@@ -14,13 +14,10 @@ import ca.mcgill.cs.stg.jetuml.graph.ClassNode;
 import ca.mcgill.cs.stg.jetuml.graph.Graph;
 import ca.mcgill.cs.stg.jetuml.graph.Node;
 
-/*
- * Note that there is code duplication between removeNode(), insertNode() and layout(). 
- * The code sets aNeedsLayout to be true twice.
- */
 public class TestDeleteNodeCommand {
     private Graph aGraph;
     private Field aNeedsLayout;
+    private Field aNodesToBeRemoved;
     private Node aNode;
     private ArrayList<Node> aRootNodes;
     private DeleteNodeCommand aDeleteNodeCommand;
@@ -32,18 +29,26 @@ public class TestDeleteNodeCommand {
         aGraph = new ClassDiagramGraph();
         aRootNodes = (ArrayList<Node>) aGraph.getRootNodes();
         aNode = new ClassNode();
-        aGraph.addNode(aNode , new Point2D.Double());
+        aGraph.addNode(aNode, new Point2D.Double());
         aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
         aNeedsLayout.setAccessible(true);
+        aNodesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aNodesToBeRemoved");
+        aNodesToBeRemoved.setAccessible(true);
         aDeleteNodeCommand = new DeleteNodeCommand(aGraph, aNode);  
-        
     }
     
     @Test
     public void testExecute() {
         aDeleteNodeCommand.execute();
         hasExecuted = true;
-        assertTrue(aGraph.contains(aNode));
+        try {
+            ArrayList<Node> aListNodesToBeRemoved = (ArrayList<Node>)(aNodesToBeRemoved.get(aGraph));
+            assertTrue(aListNodesToBeRemoved.contains((Node)aNode));
+        } catch (IllegalArgumentException e1) {
+            e1.printStackTrace();
+        } catch (IllegalAccessException e1) {
+            e1.printStackTrace();
+        }
         try {
             assertTrue((boolean)aNeedsLayout.get(aGraph));
         } catch (IllegalArgumentException e) {
@@ -62,6 +67,7 @@ public class TestDeleteNodeCommand {
         int numOfNodes = aRootNodes.size();
         aDeleteNodeCommand.undo();
         assertEquals(numOfNodes+1, aRootNodes.size());
+
         try {
             assertTrue((boolean)aNeedsLayout.get(aGraph));
         } catch (IllegalArgumentException e) {
