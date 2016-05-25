57b8e8e056e875478ae8ef356b40b575cb709a2b
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-25 10:18:48 -0400

    activated checkStyle; removed field hasExecuted, move aRootNodes to local, encapsulated exception

--- test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
@@ -14,66 +14,65 @@ import ca.mcgill.cs.stg.jetuml.graph.ClassNode;
 import ca.mcgill.cs.stg.jetuml.graph.Graph;
 import ca.mcgill.cs.stg.jetuml.graph.Node;
 
-public class TestDeleteNodeCommand {
+public class TestDeleteNodeCommand 
+{
     private Graph aGraph;
     private Field aNeedsLayout;
     private Field aNodesToBeRemoved;
     private Node aNode;
-    private ArrayList<Node> aRootNodes;
     private DeleteNodeCommand aDeleteNodeCommand;
-    private boolean hasExecuted = false;
-    
+
     @Before
-    public void setUp() throws Exception
+    public void setUp() throws Exception 
     {
         aGraph = new ClassDiagramGraph();
-        aRootNodes = (ArrayList<Node>) aGraph.getRootNodes();
         aNode = new ClassNode();
         aGraph.addNode(aNode, new Point2D.Double());
         aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
         aNeedsLayout.setAccessible(true);
         aNodesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aNodesToBeRemoved");
         aNodesToBeRemoved.setAccessible(true);
-        aDeleteNodeCommand = new DeleteNodeCommand(aGraph, aNode);  
+        aDeleteNodeCommand = new DeleteNodeCommand(aGraph, aNode);
     }
-    
-    @Test
-    public void testExecute() {
-        aDeleteNodeCommand.execute();
-        hasExecuted = true;
-        try {
-            ArrayList<Node> aListNodesToBeRemoved = (ArrayList<Node>)(aNodesToBeRemoved.get(aGraph));
-            assertTrue(aListNodesToBeRemoved.contains((Node)aNode));
-        } catch (IllegalArgumentException e1) {
-            e1.printStackTrace();
-        } catch (IllegalAccessException e1) {
-            e1.printStackTrace();
-        }
-        try {
-            assertTrue((boolean)aNeedsLayout.get(aGraph));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } 
-    }
-    
-    @Test
-    public void testUndo() {
-        if (!hasExecuted)
-        {
-            aDeleteNodeCommand.execute();
-        }
-        int numOfNodes = aRootNodes.size();
-        aDeleteNodeCommand.undo();
-        assertEquals(numOfNodes+1, aRootNodes.size());
 
-        try {
-            assertTrue((boolean)aNeedsLayout.get(aGraph));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testExecute() 
+    {
+        aDeleteNodeCommand.execute();
+        try 
+        {
+            ArrayList<Node> aListNodesToBeRemoved = (ArrayList<Node>) (aNodesToBeRemoved.get(aGraph));
+            assertTrue(aListNodesToBeRemoved.contains((Node) aNode));
+        } 
+        catch (Exception e) 
+        {
+            fail();
+        }
+        try 
+        {
+            assertTrue((boolean) aNeedsLayout.get(aGraph));
+        } 
+        catch (Exception e) 
+        {
+            fail();
+        }
+    }
+
+    @Test
+    public void testUndo() 
+    {
+        aDeleteNodeCommand.execute();
+        int numOfNodes = aGraph.getRootNodes().size();
+        aDeleteNodeCommand.undo();
+        assertEquals(numOfNodes + 1, aGraph.getRootNodes().size());
+        try 
+        {
+            assertTrue((boolean) aNeedsLayout.get(aGraph));
+        } 
+        catch (Exception e) 
+        {
+            fail();
         }
     }
 }
