bc1a85d8aaae805cbae262fc9c2ddd7212ad5843
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-25 13:48:48 -0400

    removed field hasExecuted, move aRootNodes to local, encapsulated exception

--- test/ca/mcgill/cs/stg/jetuml/commands/TestRemoveEdgeCommandTest.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestRemoveEdgeCommandTest.java
@@ -10,72 +10,76 @@ import org.junit.Test;
 
 import ca.mcgill.cs.stg.jetuml.diagrams.ClassDiagramGraph;
 import ca.mcgill.cs.stg.jetuml.graph.CallEdge;
-import ca.mcgill.cs.stg.jetuml.graph.ClassNode;
 import ca.mcgill.cs.stg.jetuml.graph.Edge;
 import ca.mcgill.cs.stg.jetuml.graph.Graph;
-import ca.mcgill.cs.stg.jetuml.graph.Node;
 
-public class TestRemoveEdgeCommandTest {
+public class TestRemoveEdgeCommandTest 
+{
     private Graph aGraph;
     private Field aNeedsLayout;
     private Field aEdgesToBeRemoved;
-    Edge aEdge;
-    private ArrayList<Edge> aEdges;
+    private Edge aEdge;
     private RemoveEdgeCommand aRemoveEdgeCommand;
-    private boolean hasExecuted = false;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception 
+    {
         aGraph = new ClassDiagramGraph();
         aEdge = new CallEdge();
-        aEdges = (ArrayList<Edge>) aGraph.getEdges();
-        aEdges.add(aEdge);
         aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
         aNeedsLayout.setAccessible(true);
         aEdgesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aEdgesToBeRemoved");
         aEdgesToBeRemoved.setAccessible(true);
-        
-        aRemoveEdgeCommand = new RemoveEdgeCommand(aGraph, aEdge);  
+        aRemoveEdgeCommand = new RemoveEdgeCommand(aGraph, aEdge);
     }
 
     @SuppressWarnings("unchecked")
     @Test
-    public void testExecute() {
+    public void testExecute() 
+    {
         aRemoveEdgeCommand.execute();
-        hasExecuted = true;
-        try {
-            ArrayList<Edge> aListEdgesToBeRemoved = (ArrayList<Edge>) aEdgesToBeRemoved.get(aGraph);
-            assertTrue(aListEdgesToBeRemoved.contains(aEdge));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-      
-        try {
-            assertTrue((boolean)aNeedsLayout.get(aGraph));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    @Test
-    public void testUndo(){
-        if (!hasExecuted)
+        ArrayList<Edge> aListEdgesToBeRemoved;
+        try 
         {
-            aRemoveEdgeCommand.execute();
+            aListEdgesToBeRemoved = (ArrayList<Edge>) aEdgesToBeRemoved.get(aGraph);
+            assertTrue(aListEdgesToBeRemoved.contains(aEdge));
+        } 
+        catch (IllegalArgumentException | IllegalAccessException e1) 
+        {
+            fail();
+        }
+        try 
+        {
+            assertTrue((boolean) aNeedsLayout.get(aGraph));
+        } 
+        catch (IllegalArgumentException | IllegalAccessException e) 
+        {
+            fail();
+        }
+
+    }
+
+    @Test
+    public void testUndo() 
+    {
+        aRemoveEdgeCommand.execute();
+        try 
+        {
+            aNeedsLayout.set(aGraph, false);
+        } 
+        catch (IllegalArgumentException | IllegalAccessException e1) 
+        {
+            fail();
         }
         aRemoveEdgeCommand.undo();
-        assertTrue(aEdges.contains(aEdge));
-        try {
-            assertTrue((boolean)aNeedsLayout.get(aGraph));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        assertTrue(aGraph.getEdges().contains(aEdge));
+        try 
+        {
+            assertTrue((boolean) aNeedsLayout.get(aGraph));
+        } 
+        catch (IllegalArgumentException | IllegalAccessException e) 
+        {
+            fail();
         }
     }
 
