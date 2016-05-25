061619d10014bd13fa2055b6cbf20a2b56c7a242
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-25 11:41:05 -0400

    delete hasExecuted

--- test/ca/mcgill/cs/stg/jetuml/commands/TestAddNodeCommand.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestAddNodeCommand.java
@@ -19,14 +19,11 @@ public class TestAddNodeCommand {
     private Field aNeedsLayout;
     private Field aNodesToBeRemoved;
     private Node aNode;
-    private ArrayList<Node> aRootNodes;
     private AddNodeCommand aAddNodeCommand;
-    private boolean hasExecuted = false;
 
     @Before
     public void setUp() throws Exception {
         aGraph = new ClassDiagramGraph();
-        aRootNodes = (ArrayList<Node>) aGraph.getRootNodes();
         aNode = new ClassNode();
         aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
         aNeedsLayout.setAccessible(true);
@@ -37,10 +34,9 @@ public class TestAddNodeCommand {
 
     @Test
     public void testExecute() {
-        int numOfNodes = aRootNodes.size();
+        assertEquals(0, aGraph.getRootNodes().size());
         aAddNodeCommand.execute();
-        hasExecuted = true;
-        assertEquals(numOfNodes+1, aRootNodes.size());
+        assertEquals(1, aGraph.getRootNodes().size());
         try {
             assertTrue((boolean)aNeedsLayout.get(aGraph));
         } catch (IllegalArgumentException e) {
@@ -52,10 +48,9 @@ public class TestAddNodeCommand {
     
     @Test
     public void testUndo() {
-        if (!hasExecuted)
-        {
-            aAddNodeCommand.execute();
-        }
+
+        aAddNodeCommand.execute();
+
         aAddNodeCommand.undo();
         try {
             ArrayList<Node> aListNodesToBeRemoved = (ArrayList<Node>)(aNodesToBeRemoved.get(aGraph));
