78115bfb1da830dbe90efbb0b5a27d923e5f3e16
Charlotte <charlottexiaoyeding@gmail.com>
2016-05-25 15:38:17 -0400

    remove layout()

--- test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
+++ test/ca/mcgill/cs/stg/jetuml/commands/TestDeleteNodeCommand.java
@@ -2,7 +2,6 @@ package ca.mcgill.cs.stg.jetuml.commands;
 
 import static org.junit.Assert.*;
 
-import java.awt.geom.Point2D;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
@@ -17,7 +16,6 @@ import ca.mcgill.cs.stg.jetuml.graph.Node;
 public class TestDeleteNodeCommand 
 {
     private Graph aGraph;
-    private Field aNeedsLayout;
     private Field aNodesToBeRemoved;
     private Node aNode;
     private DeleteNodeCommand aDeleteNodeCommand;
@@ -27,8 +25,6 @@ public class TestDeleteNodeCommand
     {
         aGraph = new ClassDiagramGraph();
         aNode = new ClassNode();
-        aNeedsLayout = aGraph.getClass().getSuperclass().getDeclaredField("aNeedsLayout");
-        aNeedsLayout.setAccessible(true);
         aNodesToBeRemoved = aGraph.getClass().getSuperclass().getDeclaredField("aNodesToBeRemoved");
         aNodesToBeRemoved.setAccessible(true);
         aDeleteNodeCommand = new DeleteNodeCommand(aGraph, aNode);
@@ -49,38 +45,14 @@ public class TestDeleteNodeCommand
         {
             fail();
         }
-        try 
-        {
-            assertTrue((boolean) aNeedsLayout.get(aGraph));
-        } 
-        catch (IllegalArgumentException | IllegalAccessException e) 
-        {
-            fail();
-        }
     }
 
     @Test
     public void testUndo() 
     {
         aDeleteNodeCommand.execute();
-        try 
-        {
-            aNeedsLayout.set(aGraph, false);
-        } 
-        catch (IllegalArgumentException | IllegalAccessException e1) 
-        {
-            fail();
-        }
         assertEquals(0, aGraph.getRootNodes().size());
         aDeleteNodeCommand.undo();
         assertEquals(1, aGraph.getRootNodes().size());
-        try 
-        {
-            assertTrue((boolean) aNeedsLayout.get(aGraph));
-        } 
-        catch (IllegalArgumentException | IllegalAccessException e) 
-        {
-            fail();
-        }
     }
 }
