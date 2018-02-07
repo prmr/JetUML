b15f193feffb6d6339143d3d47a4b994915e806c
Kaylee Kutschera <kaylee.kutschera@mail.mcgill.ca>
2018-02-07 11:26:38 -0500

    Port tests to new design

---
+++ test/ca/mcgill/cs/jetuml/application/TestFileExtensions.java
@@ -0,0 +1,40 @@
+package ca.mcgill.cs.jetuml.application;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class TestFileExtensions 
+{
+	
+	@Test
+	public void testGetAll() 
+	{
+		List<ExtensionFilter> filters = FileExtensions.getAll();
+		assertEquals(7, filters.size());
+		for(ExtensionFilter filter : filters) 
+		{
+			assertNotNull(filter);
+		}
+	}
+	
+	@Test
+	public void testGetOnValidInput() 
+	{
+		assertNotNull(FileExtensions.get("Class Diagram Files"));
+		assertNotNull(FileExtensions.get("Jet Files"));
+		assertNotNull(FileExtensions.get("All Files"));
+	}
+	
+	@Test
+	public void testGetOnInvalidInput() 
+	{
+		assertEquals(null, FileExtensions.get(""));
+	}
+
+}
--- test/ca/mcgill/cs/jetuml/gui/TestEditorFrame.java
+++ test/ca/mcgill/cs/jetuml/gui/TestEditorFrame.java
@@ -21,98 +21,16 @@
 package ca.mcgill.cs.jetuml.gui;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import javax.swing.filechooser.FileFilter;
-
-import org.junit.Before;
 import org.junit.Test;
 
-import ca.mcgill.cs.jetuml.UMLEditor;
-import ca.mcgill.cs.jetuml.gui.EditorFrame;
-import junit.framework.TestCase;
-
 public class TestEditorFrame 
-{ 
-	private Method aCreateFileFilter;
-	private EditorFrame aEditorFrame; 
-	
-	@Before
-	public void setup() throws Exception
-	{
-		aCreateFileFilter = EditorFrame.class.getDeclaredMethod("createFileFilter", String.class);
-		aCreateFileFilter.setAccessible(true);
-		aEditorFrame = new EditorFrame(UMLEditor.class, null);
-	}
-	
+{ 	
 	@Test
 	public void testReplaceExtension()
 	{
 		assertEquals("foo.png", EditorFrame.replaceExtension("foo.jet", ".jet", ".png"));
 		assertEquals("", EditorFrame.replaceExtension("", ".jet", ".png"));
 		assertEquals("foo.class.png", EditorFrame.replaceExtension("foo.class.jet", ".jet", ".png"));
-	}
-	
-	@Test
-	public void testCreateFileFilterToString()
-	{
-		FileFilter filter = createFileFilter("PNG");
-		assertEquals("PNG", filter.toString());
-	}
-	
-	@Test
-	public void testCreateFileFilterAcceptFileUpperCase()
-	{
-		FileFilter filter = createFileFilter("PNG");
-		File temp = new File("foo.PNG");
-		assertTrue(filter.accept(temp));
-		temp.delete();
-	}
-	
-	@Test
-	public void testCreateFileFilterAcceptFileLowerCase()
-	{
-		FileFilter filter = createFileFilter("PNG");
-		File temp = new File("foo.png");
-		assertTrue(filter.accept(temp));
-		temp.delete();
-	}
-	
-	@Test
-	public void testCreateFileFilteRejectFile()
-	{
-		FileFilter filter = createFileFilter("PNG");
-		File temp = new File("foo.gif");
-		assertFalse(filter.accept(temp));
-		temp.delete();
-	}
-	
-	@Test
-	public void testCreateFileFilteAcceptDirectory()
-	{
-		FileFilter filter = createFileFilter("PNG");
-		File temp = new File("foo");
-		temp.mkdir();
-		assertTrue(temp.isDirectory());
-		assertTrue(filter.accept(temp));
-		temp.delete();
-	}
-	
-	private FileFilter createFileFilter(String pFormat)
-	{
-		try
-		{
-			return (FileFilter) aCreateFileFilter.invoke(aEditorFrame, pFormat);
-		}
-		catch(InvocationTargetException | IllegalAccessException pException)
-		{
-			TestCase.fail();
-			return null;
-		}
 	}
 }
