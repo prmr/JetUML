035bc19749fd05a04ba7f0fe3132f7c757c9bb41
prmr <prmr@gmx.com>
2018-07-10 16:44:57 -0400

    #265 Add tests

--- test/ca/mcgill/cs/jetuml/application/TestClipboard2.java
+++ test/ca/mcgill/cs/jetuml/application/TestClipboard2.java
@@ -22,13 +22,12 @@ package ca.mcgill.cs.jetuml.application;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Before;
@@ -37,20 +36,14 @@ import org.junit.Test;
 
 import ca.mcgill.cs.jetuml.JavaFXLoader;
 import ca.mcgill.cs.jetuml.diagram.ClassDiagram;
-import ca.mcgill.cs.jetuml.diagram.DiagramElement;
 import ca.mcgill.cs.jetuml.diagram.Edge;
 import ca.mcgill.cs.jetuml.diagram.Node;
-import ca.mcgill.cs.jetuml.diagram.edges.CallEdge;
 import ca.mcgill.cs.jetuml.diagram.edges.DependencyEdge;
-import ca.mcgill.cs.jetuml.diagram.nodes.ChildNode;
 import ca.mcgill.cs.jetuml.diagram.nodes.ClassNode;
-import ca.mcgill.cs.jetuml.diagram.nodes.ImplicitParameterNode;
+import ca.mcgill.cs.jetuml.diagram.nodes.FieldNode;
+import ca.mcgill.cs.jetuml.diagram.nodes.ObjectNode;
 import ca.mcgill.cs.jetuml.diagram.nodes.PackageNode;
 import ca.mcgill.cs.jetuml.geom.Point;
-import ca.mcgill.cs.jetuml.gui.DiagramCanvas;
-import ca.mcgill.cs.jetuml.gui.DiagramCanvasController;
-import ca.mcgill.cs.jetuml.gui.DiagramTabToolBar;
-import ca.mcgill.cs.jetuml.gui.SelectionModel;
 
 public class TestClipboard2
 {
@@ -58,6 +51,8 @@ public class TestClipboard2
 	private Field aNodesField;
 	private Field aEdgesField;
 	private ClassNode aNode1;
+	private ClassNode aNode2;
+	private ClassDiagram aDiagram;
 	
 	public TestClipboard2() throws ReflectiveOperationException
 	{
@@ -65,6 +60,16 @@ public class TestClipboard2
 		aNodesField.setAccessible(true);
 		aEdgesField = Clipboard2.class.getDeclaredField("aEdges");
 		aEdgesField.setAccessible(true);
+	}
+	
+	/**
+	 * Load JavaFX toolkit and environment.
+	 */
+	@BeforeClass
+	@SuppressWarnings("unused")
+	public static void setupClass()
+	{
+		JavaFXLoader loader = JavaFXLoader.instance();
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -101,6 +106,8 @@ public class TestClipboard2
 	{
 		aClipboard = Clipboard2.instance();		
 		aNode1 = new ClassNode();
+		aNode2 = new ClassNode();
+		aDiagram = new ClassDiagram();
 	}
 	
 	@Test
@@ -120,5 +127,86 @@ public class TestClipboard2
 		assertEquals(1, getClipboardNodes().size());
 		assertFalse(getClipboardNodes().contains(aNode1)); // Clone
 		assertEquals(new Point(0,0), getClipboardNodes().get(0).position());
+	}
+	
+	@Test
+	public void testCopyTwoNodesOneEdgeFlat()
+	{
+		aNode1.translate(10, 10);
+		aNode2.translate(200, 200);
+		DependencyEdge edge = new DependencyEdge();
+		edge.connect(aNode1, aNode2, aDiagram);
+		aClipboard.copy(Arrays.asList(aNode1, aNode2, edge));
+		List<Node> nodes = getClipboardNodes();
+		assertEquals(2, nodes.size());
+		assertEquals(new Point(0,0), nodes.get(0).position());
+		assertEquals(new Point(190,190), nodes.get(1).position());
+		List<Edge> edges = getClipboardEdges();
+		assertEquals(1, edges.size());
+		assertTrue(edges.get(0).getStart() == nodes.get(0));
+		assertTrue(edges.get(0).getEnd() == nodes.get(1));
+	}
+	
+	@Test
+	public void testCopyDanglingEdgeFlat()
+	{
+		aNode1.translate(10, 10);
+		aNode2.translate(200, 200);
+		DependencyEdge edge = new DependencyEdge();
+		edge.connect(aNode1, aNode2, aDiagram);
+		aClipboard.copy(Arrays.asList(aNode1, edge));
+		List<Node> nodes = getClipboardNodes();
+		assertEquals(1, nodes.size());
+		assertEquals(new Point(0,0), nodes.get(0).position());
+		List<Edge> edges = getClipboardEdges();
+		assertEquals(0, edges.size());
+	}
+	
+	@Test
+	public void testCopyNodeWithOneChild()
+	{
+		PackageNode pn = new PackageNode();
+		pn.addChild(aNode1);
+		DependencyEdge edge = new DependencyEdge();
+		edge.connect(aNode1, aNode1, aDiagram);
+		aClipboard.copy(Arrays.asList(pn));
+		List<Node> nodes = getClipboardNodes();
+		assertEquals(1, nodes.size());
+		PackageNode node = (PackageNode)nodes.get(0);
+		assertEquals(1, node.getChildren().size());
+		assertEquals(new Point(0,0), nodes.get(0).position());
+		List<Edge> edges = getClipboardEdges();
+		assertEquals(0, edges.size());
+	}
+	
+	@Test
+	public void testCopyNodeWithOneParent()
+	{
+		PackageNode pn = new PackageNode();
+		pn.addChild(aNode1);
+		DependencyEdge edge = new DependencyEdge();
+		edge.connect(aNode1, aNode1, aDiagram);
+		aClipboard.copy(Arrays.asList(aNode1));
+		List<Node> nodes = getClipboardNodes();
+		assertEquals(1, nodes.size());
+		ClassNode node = (ClassNode)nodes.get(0);
+		assertNull(node.getParent());
+		assertEquals(new Point(0,0), nodes.get(0).position());
+		List<Edge> edges = getClipboardEdges();
+		assertEquals(0, edges.size());
+	}
+	
+	@Test
+	public void testCopyNodeMissingParent()
+	{
+		ObjectNode node = new ObjectNode();
+		FieldNode field = new FieldNode();
+		field.setName("Foo");
+		field.setValue("Bar");
+		node.addChild(field);
+		
+		aClipboard.copy(Arrays.asList(field));
+		List<Node> nodes = getClipboardNodes();
+		assertEquals(0, nodes.size());
 	}
 }
