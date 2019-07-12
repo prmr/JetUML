c7bbe283eb482c6df4ed308399b1a3ca34005d2d
prmr <prmr@gmx.com>
2019-07-12 12:58:41 -0400

    #307 Create new stateless viewer stategies

---
+++ src/ca/mcgill/cs/jetuml/views/DiagramViewer.java
@@ -0,0 +1,207 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2018 by the contributors of the JetUML project.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package ca.mcgill.cs.jetuml.views;
+
+import java.util.Optional;
+
+import ca.mcgill.cs.jetuml.diagram.Diagram;
+import ca.mcgill.cs.jetuml.diagram.DiagramElement;
+import ca.mcgill.cs.jetuml.diagram.Edge;
+import ca.mcgill.cs.jetuml.diagram.Node;
+import ca.mcgill.cs.jetuml.diagram.nodes.ParentNode;
+import ca.mcgill.cs.jetuml.geom.Point;
+import ca.mcgill.cs.jetuml.geom.Rectangle;
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * A strategy for drawing a diagram and computing geometric properties of a 
+ * diagram. This class can be inherited if certain diagram types require specialized 
+ * services. This class is stateless.
+ */
+public class DiagramViewer
+{
+	/**
+	 * Draws pDiagram onto pGraphics.
+	 * 
+	 * @param pGraphics the graphics context where the
+	 * diagram should be drawn.
+	 * @param pDiagram the diagram to draw.
+	 * @pre pDiagram != null && pGraphics != null.
+	 */
+	public final void draw(Diagram pDiagram, GraphicsContext pGraphics)
+	{
+		assert pDiagram != null && pGraphics != null;
+		pDiagram.rootNodes().forEach(node -> drawNode(node, pGraphics));
+		pDiagram.edges().forEach(edge -> edge.view().draw(pGraphics));
+	}
+	
+	private void drawNode(Node pNode, GraphicsContext pGraphics)
+	{
+		pNode.view().draw(pGraphics);
+		if(pNode instanceof ParentNode)
+		{
+			((ParentNode)pNode).getChildren().forEach(node -> drawNode(node, pGraphics));
+		}
+	}
+	
+	/**
+	 * Finds the edge that contains the given point, if it 
+	 * exists.
+	 * 
+	 * @param pDiagram The diagram to query
+	 * @param pPoint a point
+	 * @return An edge containing pPoint or null if no edge contains pPoint
+	 * @pre pDiagram != null && pPoint != null
+	 */
+	public final Optional<Edge> findEdge(Diagram pDiagram, Point pPoint)
+	{
+		assert pDiagram != null && pPoint != null;
+		for(Edge edge : pDiagram.edges())
+		{
+			if(edge.view().contains(pPoint))
+			{
+				return Optional.of(edge);
+			}
+		}
+		return Optional.empty();
+	}
+	
+	/**
+     * Finds a node that contains the given point. Always returns
+     * the deepest child and the last one in a list.
+     * @param pDiagram The diagram to query.
+     * @param pPoint A point
+     * @return a node containing pPoint or null if no nodes contain pPoint
+     * @pre pDiagram != null && pPoint != null.
+     */
+	public final Optional<Node> findNode(Diagram pDiagram, Point pPoint)
+	{
+		assert pDiagram != null && pPoint != null;
+		Node result = null;
+		for(Node node : pDiagram.rootNodes())
+		{
+			Node temp = deepFindNode(pDiagram, node, pPoint);
+			if (temp != null)
+			{
+				result = temp;
+			}
+		}
+		return Optional.ofNullable(result);
+	}
+	
+	/**
+	 * Find the "deepest" child that contains pPoint,
+	 * where depth is measured in terms of distance from
+	 * pNode along the parent-child relation.
+	 * @param pDiagram The diagram to query.
+	 * @param pNode The starting node for the search.
+	 * @param pPoint The point to test for.
+	 * @return The deepest child containing pPoint,
+	 * or null if pPoint is not contained by pNode or 
+	 * any of its children.
+	 * @pre pNode != null, pPoint != null;
+	 */
+	protected Node deepFindNode(Diagram pDiagram, Node pNode, Point pPoint)
+	{
+		assert pDiagram != null && pNode != null && pPoint != null;
+		Node node = null;
+		if (pNode instanceof ParentNode)
+		{
+			for (Node child : ((ParentNode) pNode).getChildren())
+			{
+				node = deepFindNode(pDiagram, child, pPoint);
+				if(node != null)
+				{
+					return node;
+				}
+			}
+		}
+		if (pNode.view().contains(pPoint))
+		{
+			return pNode;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * Gets the smallest rectangle enclosing the diagram.
+	 * 
+	 * @param pDiagram The diagram to query
+	 * @return The bounding rectangle
+	 * @pre pDiagram != null
+	 */
+	public final Rectangle getBounds(Diagram pDiagram)
+	{
+		assert pDiagram != null;
+		Rectangle bounds = null;
+		for(Node node : pDiagram.rootNodes() )
+		{
+			if(bounds == null)
+			{
+				bounds = node.view().getBounds();
+			}
+			else
+			{
+				bounds = bounds.add(node.view().getBounds());
+			}
+		}
+		for(Edge edge : pDiagram.edges())
+		{
+			bounds = bounds.add(edge.view().getBounds());
+		}
+		if(bounds == null )
+		{
+			return new Rectangle(0, 0, 0, 0);
+		}
+		else
+		{
+			return new Rectangle(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+		}
+	}
+	
+	/**
+	 * Used during pasting to determine whether the current selection bounds completely overlaps the new elements.
+	 * @param pCurrentSelectionBounds The current selection bounds
+	 * @param pNewElements Elements to be pasted
+	 * @return Is the current selection bounds completely overlapping the new elements
+	 */
+	public boolean isOverlapping(Rectangle pCurrentSelectionBounds, Iterable<DiagramElement> pNewElements) 
+	{
+		Rectangle newElementBounds = null;
+		for (DiagramElement element : pNewElements) 
+		{
+			if (newElementBounds == null) 
+			{
+				newElementBounds = element.view().getBounds();
+			}
+			newElementBounds = newElementBounds.add(element.view().getBounds());
+		}
+		if (pCurrentSelectionBounds.equals(newElementBounds)) 
+		{
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/ca/mcgill/cs/jetuml/views/SequenceDiagramViewer.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2018 by the contributors of the JetUML project.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package ca.mcgill.cs.jetuml.views;
+
+import ca.mcgill.cs.jetuml.diagram.ControlFlow;
+import ca.mcgill.cs.jetuml.diagram.Diagram;
+import ca.mcgill.cs.jetuml.diagram.DiagramElement;
+import ca.mcgill.cs.jetuml.diagram.Node;
+import ca.mcgill.cs.jetuml.diagram.SequenceDiagram;
+import ca.mcgill.cs.jetuml.diagram.nodes.CallNode;
+import ca.mcgill.cs.jetuml.geom.Point;
+import ca.mcgill.cs.jetuml.geom.Rectangle;
+
+/**
+ * A specialized viewer stategy for a sequence diagram.
+ */
+public class SequenceDiagramViewer extends DiagramViewer
+{
+	@Override
+	protected Node deepFindNode(Diagram pDiagram, Node pNode, Point pPoint )
+	{		
+		ControlFlow flow = new ControlFlow((SequenceDiagram)pDiagram);
+		if( pNode instanceof CallNode )
+		{
+			for(Node child : flow.getCallees(pNode))
+			{
+				if ( child != null )
+				{
+					Node node = deepFindNode(pDiagram, child, pPoint);
+					if ( node != null )
+					{
+						return node;
+					}
+				}
+			}
+		}
+		return super.deepFindNode(pDiagram, pNode, pPoint);
+	}
+	
+	/*
+	 * Used during pasting to determine whether the current selection bounds completely overlaps the new elements.
+	 * For sequence diagrams the height between the selection bounds and the bounds of the new elements may vary, but 
+	 * the height is irrelevant to determining overlap.
+	 * 
+	 * @param pCurrentSelectionBounds The current selection bounds
+	 * @param pNewElements Elements to be pasted
+	 * @return Is the current selection bounds overlapping the new elements
+	 */
+	@Override
+	public boolean isOverlapping(Rectangle pCurrentSelectionBounds, Iterable<DiagramElement> pNewElements) 
+	{
+		Rectangle newElementBounds = null;
+		for (DiagramElement element : pNewElements) 
+		{
+			if (newElementBounds == null) 
+			{
+				newElementBounds = element.view().getBounds();
+			}
+			newElementBounds = newElementBounds.add(element.view().getBounds());
+		}
+		if (newElementBounds == null)
+		{
+			return false;
+		}
+		if (pCurrentSelectionBounds.getX() == newElementBounds.getX() && 
+				pCurrentSelectionBounds.getY() == newElementBounds.getY() && 
+				pCurrentSelectionBounds.getWidth() == newElementBounds.getWidth())
+		{
+			return true;
+		}
+		return false;
+	}
+}
