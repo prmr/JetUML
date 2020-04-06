2a174393d22a3a79d1c3558906c5a456c28ccae3
prmr <prmr@gmx.com>
2020-04-06 15:16:49 -0400

    #386 Fix copyrights

--- test/ca/mcgill/cs/jetuml/viewers/nodes/TestTypeNodeViewer.java
+++ test/ca/mcgill/cs/jetuml/viewers/nodes/TestTypeNodeViewer.java
@@ -1,13 +1,22 @@
 /*******************************************************************************
- * Fraco - Eclipse plug-in to detect fragile comments
+ * JetUML - A desktop application for fast UML diagramming.
  *
- * Copyright (C) 2020 McGill University
- *     
- * Eclipse Public License - v 2.0
+ * Copyright (C) 2015-2019 by the contributors of the JetUML project.
  *
- *  THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE
- *  PUBLIC LICENSE v2.0. ANY USE, REPRODUCTION OR DISTRIBUTION
- *  OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 package ca.mcgill.cs.jetuml.viewers.nodes;
 
