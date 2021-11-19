5f42c02c4d94c5a6101b869542aae98d0761af46
delphinepilon <delphine.pilon@icloud.com>
2021-11-19 14:07:55 -0500

    Add unit tests.
    Added unit tests to increase the code coverage of SegmentedEdgeViewer.getAttachmentPoint(...). All lines of code are now covered.

--- test/ca/mcgill/cs/jetuml/viewers/edges/TestAssociationEdgeViewer.java
+++ test/ca/mcgill/cs/jetuml/viewers/edges/TestAssociationEdgeViewer.java
@@ -8,11 +8,14 @@ import java.lang.reflect.Method;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import ca.mcgill.cs.jetuml.geom.Rectangle;
+import ca.mcgill.cs.jetuml.views.ArrowHead;
 import javafx.geometry.Point2D;
 
 public class TestAssociationEdgeViewer 
 {
 	private AssociationEdgeViewer aAssociationEdgeViewer = new AssociationEdgeViewer();
+	private ArrowHead aArrowHead = ArrowHead.DIAMOND;
 	
 	@ParameterizedTest
 	@CsvSource(value = {
@@ -30,6 +33,35 @@ public class TestAssociationEdgeViewer
 		assertEquals(pExpectedNumberOfLines, numberOfLines);
 	}
 	
+	@ParameterizedTest
+	@CsvSource(value = {
+			"0, 0, 0, 100, 15, 15, false, false, 9, 82",
+			"0, 0, 0, 100, 15, 15, true, false, 3, 32",
+			"0, 0, 0, 100, 1, 1, true, false, 3, 46",
+			"100, 0, 0, 100, 1, 1, true, false, 53, 46",
+			"0, 100, 0, 0, 1, 1, false, false, 9, 3",
+			"100, 500, 200, 500, 15, 15, false, false, 182, 475",
+			"100, 500, 200, 500, 15, 15, true, false, 143, 482",
+			"100, 500, 200, 500, 1, 1, true, false, 150, 496",
+			"100, 500, 200, 500, 150, 150, true, false, 203, 347",
+			"100, 500, 200, 500, 10, 10, true, true, 145, 487",
+			"100, 100, 100, 500, 10, 10, true, true, 103, 303"
+	})
+	public void testGetAttachmentPoint(int pPoint1X, int pPoint1Y, int pPoint2X, int pPoint2Y, 
+			int pTextDimensionWidth, int pTextDimensionHeight, boolean pCenter, 
+			boolean pIsStepUp, int pExpectedX, int pExpectedY)
+	{
+		Point2D point1 = new Point2D(pPoint1X, pPoint1Y);
+		Point2D point2 = new Point2D(pPoint2X, pPoint2Y);
+		Rectangle textBounds = new Rectangle(0, 0, pTextDimensionWidth, pTextDimensionHeight);
+		Point2D result = getAttachmentPoint(point1, point2, aArrowHead, textBounds, pCenter, pIsStepUp);
+		assertEquals(pExpectedX, (int)result.getX());
+		assertEquals(pExpectedY, (int)result.getY());
+	}
+	
+	/**
+	 * Calls the private method SegmentedEdgeViewer.wrapLabel(...) with the given parameters. 
+	 */
 	private String wrapLabel(String pString, Point2D pPoint1, Point2D pPoint2) 
 	{
 		try 
@@ -44,6 +76,29 @@ public class TestAssociationEdgeViewer
 			assert false;
 			fail();
 			return "";
+		}
+	}
+	
+	/**
+	 * Calls the private method SegmentedEdgeViewer.getAttachmentPoint(...) with the given parameters. 
+	 */
+	private Point2D getAttachmentPoint(Point2D pEndPoint1, Point2D pEndPoint2, 
+			ArrowHead pArrow, Rectangle pDimension, boolean pCenter, boolean pIsStepUp) 
+	{
+		try 
+		{
+			Method method = SegmentedEdgeViewer.class.getDeclaredMethod("getAttachmentPoint", 
+					Point2D.class, Point2D.class, ArrowHead.class, Rectangle.class, boolean.class, boolean.class);
+			method.setAccessible(true);
+			Point2D point = (Point2D)method.invoke(aAssociationEdgeViewer, 
+					pEndPoint1, pEndPoint2, pArrow, pDimension, pCenter, pIsStepUp);
+			return point;
+		} 
+		catch (ReflectiveOperationException e)
+		{
+			assert false;
+			fail();
+			return null;
 		}
 	}
 }
