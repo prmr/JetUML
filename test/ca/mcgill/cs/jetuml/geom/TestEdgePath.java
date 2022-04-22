865fe2b54436e5ea2e3e1db260209a5139b60896
Maddy Lecsek <madelyn.lecsek@mail.mcgill.ca>
2022-04-22 12:49:12 -0400

    #375 Add EdgePath and tests
    Add an EdgePath which represents the path of an edge as a list of Point.
    Add tests for EdgePath methods.

---
+++ test/ca/mcgill/cs/jetuml/geom/TestEdgePath.java
@@ -0,0 +1,71 @@
+package ca.mcgill.cs.jetuml.geom;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for the EdgePath class
+ */
+public class TestEdgePath 
+{
+	private final Point pointA = new Point(0,0);
+	private final Point pointB = new Point(200,0);
+	private final Point pointC = new Point(200,200);
+	private final Point pointD = new Point(300,200);
+	private EdgePath aEdgePath;
+	
+	@BeforeEach
+	public void setUp()
+	{
+		aEdgePath = new EdgePath(pointA, pointB, pointC, pointD);
+	}
+	
+	@Test
+	public void testGetStartPoint()
+	{
+		assertSame(pointA, aEdgePath.getStartPoint());
+	}
+	
+	@Test
+	public void testGetEndPoint()
+	{
+		assertSame(pointD, aEdgePath.getEndPoint());
+	}
+	
+	@Test
+	public void testGetPointByIndex()
+	{
+		assertSame(pointA, aEdgePath.getPointByIndex(0));
+		assertSame(pointB, aEdgePath.getPointByIndex(1));
+		assertSame(pointC, aEdgePath.getPointByIndex(2));
+		assertSame(pointD, aEdgePath.getPointByIndex(3));
+	}
+	
+	
+	@Test
+	public void testEquals()
+	{
+		EdgePath samePath = new EdgePath(pointA, pointB, new Point(200, 200), pointD);
+		EdgePath reversePath = new EdgePath(pointD, pointC, pointB, pointA);
+		EdgePath nullEdgePath = null;
+		assertTrue(samePath.equals(samePath));
+		assertTrue(samePath.equals(aEdgePath));
+		assertFalse(reversePath.equals(aEdgePath));
+		assertFalse(samePath.equals(nullEdgePath));
+		assertFalse(samePath.equals(new Point(0, 0)));
+		assertFalse(samePath.equals(new EdgePath(pointA, pointB, new Point(200, 200))));
+	}
+	
+	
+	
+	 @Test
+	 public void testSize()
+	 {
+		 assertTrue(aEdgePath.size() == 4);
+	 }
+	 
+	
+}
