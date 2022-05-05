e28f3189b21c57c45ae310d00e8b12fc256c9f3e
Martin Robillard <prmr@gmx.com>
2022-05-05 14:42:10 -0400

    Issue #461 Update version, add missing copyrights

--- test/ca/mcgill/cs/jetuml/viewers/TestEdgePriority.java
+++ test/ca/mcgill/cs/jetuml/viewers/TestEdgePriority.java
@@ -1,5 +1,24 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2022 by McGill University.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *******************************************************************************/
 package ca.mcgill.cs.jetuml.viewers;
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -32,8 +51,7 @@ public class TestEdgePriority
 	private Edge aSelfEdge = new AggregationEdge(AggregationEdge.Type.Composition);
 	private Edge aNoteEdge = new NoteEdge();
 	private Node aNode1 = new ClassNode();
-	
-	
+
 	@BeforeEach
 	private void setUp()
 	{
@@ -126,6 +144,4 @@ public class TestEdgePriority
 		assertTrue(EdgePriority.isStoredEdge(aSelfEdge));
 		assertFalse(EdgePriority.isStoredEdge(aNoteEdge));
 	}
-
 }
-
--- test/ca/mcgill/cs/jetuml/viewers/TestNodeCorner.java
+++ test/ca/mcgill/cs/jetuml/viewers/TestNodeCorner.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2022 by McGill University.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *******************************************************************************/
 package ca.mcgill.cs.jetuml.viewers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +35,6 @@ import ca.mcgill.cs.jetuml.viewers.edges.NodeIndex;
 
 /**
  * Tests for the enumerated type NodeCorner
- *
  */
 public class TestNodeCorner 
 {
@@ -27,7 +46,6 @@ public class TestNodeCorner
 	{
 		aDiagram.addRootNode(aNode);
 	}
-	
 
 	@Test
 	public void testGetHorizontalIndex_right()
@@ -116,6 +134,4 @@ public class TestNodeCorner
 		assertEquals(new Point(20, 60), NodeCorner.toPoints(NodeCorner.BOTTOM_LEFT, aNode)[0]);
 		assertEquals(new Point(0, 40), NodeCorner.toPoints(NodeCorner.BOTTOM_LEFT, aNode)[1]);
 	}
-	
-	
 }
