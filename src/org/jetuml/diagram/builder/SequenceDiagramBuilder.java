55e4d94e97c75896a361ff87e542a2dec48ec2dd
Laurie Prince <laurie.prince@mail.mcgill.ca>
2022-12-05 14:02:10 -0500

    Issue #484 Fix use DiagramRenderer instance

--- src/org/jetuml/diagram/builder/SequenceDiagramBuilder.java
+++ src/org/jetuml/diagram/builder/SequenceDiagramBuilder.java
@@ -248,7 +248,7 @@ public class SequenceDiagramBuilder extends DiagramBuilder
 		{
 			if( node instanceof ImplicitParameterNode && aDiagramRenderer.contains(node, pPoint) )
 			{
-				if( !(pPoint.getY() < implicitParameterNodeViewer().getTopRectangle(node).getMaxY() + CALL_NODE_YGAP) )
+				if( !(pPoint.getY() < implicitParameterRenderer().getTopRectangle(node).getMaxY() + CALL_NODE_YGAP) )
 				{
 					return Optional.of( (ImplicitParameterNode)node );
 				}
@@ -257,9 +257,9 @@ public class SequenceDiagramBuilder extends DiagramBuilder
 		return Optional.empty();
 	}
 	
-	private ImplicitParameterNodeRenderer implicitParameterNodeViewer()
+	private ImplicitParameterNodeRenderer implicitParameterRenderer()
 	{
-		return (ImplicitParameterNodeRenderer)DiagramType.newRendererInstanceFor(aDiagramRenderer.diagram()).rendererFor(ImplicitParameterNode.class);
+		return (ImplicitParameterNodeRenderer)aDiagramRenderer.rendererFor(ImplicitParameterNode.class);
 	}
 	
 	/**
