5220424af36efee8f46eededc41e99f09adf03d7
Martin Robillard <prmr@gmx.com>
2022-12-19 22:40:31 -0500

    Issue #486 Remove unused static utility method

--- src/org/json/JSONTokener.java
+++ src/org/json/JSONTokener.java
@@ -113,25 +113,6 @@ public class JSONTokener {
     }
 
     /**
-     * Get the hex value of a character (base16).
-     * @param c A character between '0' and '9' or between 'A' and 'F' or
-     * between 'a' and 'f'.
-     * @return  An int between 0 and 15, or -1 if c was not a hex digit.
-     */
-    public static int dehexchar(char c) {
-        if (c >= '0' && c <= '9') {
-            return c - '0';
-        }
-        if (c >= 'A' && c <= 'F') {
-            return c - ('A' - 10);
-        }
-        if (c >= 'a' && c <= 'f') {
-            return c - ('a' - 10);
-        }
-        return -1;
-    }
-
-    /**
      * Checks if the end of the input has been reached.
      *  
      * @return true if at the end of the file and we didn't step back
