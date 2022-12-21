e038e542c4033acc2e6a7c6b64443354cc204b72
Martin Robillard <prmr@gmx.com>
2022-12-20 21:39:12 -0500

    Issue #486 Remove unused syntaxError method

--- src/org/json/JSONTokener.java
+++ src/org/json/JSONTokener.java
@@ -325,16 +325,4 @@ public class JSONTokener
     {
         return new JSONException(message + toString());
     }
-
-    /**
-     * Make a JSONException to signal a syntax error.
-     *
-     * @param message The error message.
-     * @param causedBy The throwable that caused the error.
-     * @return  A JSONException object, suitable for throwing
-     */
-    public JSONException syntaxError(String message, Throwable causedBy) 
-    {
-        return new JSONException(message + toString(), causedBy);
-    }
 }
