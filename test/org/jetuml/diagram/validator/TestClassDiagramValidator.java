774df61b01673674377301f37c1bb3e00be299f0
Martin Robillard <prmr@gmx.com>
2023-06-26 13:18:13 -0400

    Issue #502 Add basic ObjectDiagramValidator tests

--- test/org/jetuml/diagram/validator/TestClassDiagramValidator.java
+++ test/org/jetuml/diagram/validator/TestClassDiagramValidator.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2020 by McGill University.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *******************************************************************************/
 package org.jetuml.diagram.validator;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -26,6 +46,7 @@ import org.jetuml.diagram.nodes.CallNode;
 import org.jetuml.diagram.nodes.ClassNode;
 import org.jetuml.diagram.nodes.FieldNode;
 import org.jetuml.diagram.nodes.FinalStateNode;
+import org.jetuml.diagram.nodes.ImplicitParameterNode;
 import org.jetuml.diagram.nodes.InitialStateNode;
 import org.jetuml.diagram.nodes.NoteNode;
 import org.jetuml.diagram.nodes.ObjectNode;
@@ -54,7 +75,8 @@ public class TestClassDiagramValidator
 	private static List<Node> provideInvalidNodes()
 	{
 		return List.of(new ActorNode(), new CallNode(), new FieldNode(), new FinalStateNode(), 
-				new InitialStateNode(), new ObjectNode(), new StateNode(), new UseCaseNode());
+				new ImplicitParameterNode(), new InitialStateNode(), new ObjectNode(), new StateNode(), 
+				new UseCaseNode());
 	}
 	
 	private static List<Edge> provideInvalidEdges()
--- test/org/jetuml/diagram/validator/TestObjectDiagramValidator.java
+++ test/org/jetuml/diagram/validator/TestObjectDiagramValidator.java
@@ -1,82 +1,99 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2020 by McGill University.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *******************************************************************************/
 package org.jetuml.diagram.validator;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.util.List;
 
 import org.jetuml.diagram.Diagram;
 import org.jetuml.diagram.DiagramType;
+import org.jetuml.diagram.Edge;
 import org.jetuml.diagram.Node;
+import org.jetuml.diagram.edges.AggregationEdge;
+import org.jetuml.diagram.edges.AssociationEdge;
+import org.jetuml.diagram.edges.CallEdge;
+import org.jetuml.diagram.edges.ConstructorEdge;
+import org.jetuml.diagram.edges.DependencyEdge;
+import org.jetuml.diagram.edges.GeneralizationEdge;
+import org.jetuml.diagram.edges.ReturnEdge;
+import org.jetuml.diagram.edges.StateTransitionEdge;
+import org.jetuml.diagram.edges.UseCaseAssociationEdge;
+import org.jetuml.diagram.edges.UseCaseDependencyEdge;
+import org.jetuml.diagram.edges.UseCaseGeneralizationEdge;
+import org.jetuml.diagram.nodes.ActorNode;
+import org.jetuml.diagram.nodes.CallNode;
 import org.jetuml.diagram.nodes.ClassNode;
-import org.jetuml.diagram.nodes.FieldNode;
+import org.jetuml.diagram.nodes.FinalStateNode;
+import org.jetuml.diagram.nodes.ImplicitParameterNode;
+import org.jetuml.diagram.nodes.InitialStateNode;
+import org.jetuml.diagram.nodes.InterfaceNode;
 import org.jetuml.diagram.nodes.NoteNode;
-import org.jetuml.diagram.nodes.ObjectNode;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.jetuml.diagram.nodes.PackageDescriptionNode;
+import org.jetuml.diagram.nodes.PackageNode;
+import org.jetuml.diagram.nodes.StateNode;
+import org.jetuml.diagram.nodes.UseCaseNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class TestObjectDiagramValidator
 {
-  private Diagram aDiagram;
-  private ObjectDiagramValidator aObjectDiagramValidator;
-  private FieldNode aFieldNode;
-  private ObjectNode aObjectNode;
-  private NoteNode aNoteNode1;
+	private final ObjectDiagramValidator aValidator =
+			new ObjectDiagramValidator(new Diagram(DiagramType.OBJECT));
+	private final NoteNode aNoteNode = new NoteNode();
+	
+	private Diagram diagram()
+	{
+		return aValidator.diagram();
+	}
 
-  @BeforeEach
-  public void setUp()
-  {
-    aDiagram = new Diagram(DiagramType.OBJECT);
-    aFieldNode = new FieldNode();
-    aObjectNode = new ObjectNode();
-    aNoteNode1 = new NoteNode();
-    aObjectDiagramValidator = new ObjectDiagramValidator(aDiagram);
-  }
-
-  @Test
-  public void testValidNodeHierarchy_True()
-  {
-    aDiagram.addRootNode(aObjectNode);
-    aDiagram.addRootNode(aNoteNode1);
-    assertTrue(aObjectDiagramValidator.isValid());
-  }
-
-  @Test
-  public void testValidNodeHierarchy_False()
-  {
-    aDiagram.addRootNode(aFieldNode);
-    assertFalse(aObjectDiagramValidator.isValid());
-    try
-    {
-      Field aFields = ObjectNode.class.getDeclaredField("aFields");
-      aFields.setAccessible(true);
-      ArrayList<Node> objectChildren = new ArrayList<>();
-      objectChildren.add(aNoteNode1);
-      aFields.set(aObjectNode, objectChildren);
-      aDiagram.addRootNode(aObjectNode);
-      assertFalse(aObjectDiagramValidator.isValid());
-    }
-    catch (NoSuchFieldException | IllegalAccessException e)
-    {
-      e.printStackTrace();
-    }
-  }
-
-  @Test
-  public void testValidElementName_True()
-  {
-    aDiagram.addRootNode(aObjectNode);
-    aDiagram.addRootNode(aNoteNode1);
-    assertTrue(aObjectDiagramValidator.isValid());
-  }
-
-  @Test
-  public void testValidElementName_False()
-  {
-    ClassNode aClassNode = new ClassNode();
-    aDiagram.addRootNode(aClassNode);
-    assertFalse(aObjectDiagramValidator.isValid());
-
-  }
+	private static List<Node> provideInvalidNodes()
+	{
+		return List.of(new ActorNode(), new CallNode(), new ClassNode(), new FinalStateNode(), 
+				new ImplicitParameterNode(), new InitialStateNode(), new InterfaceNode(), 
+				new PackageDescriptionNode(), new PackageNode(), new StateNode(), new UseCaseNode());
+	}
+	
+	private static List<Edge> provideInvalidEdges()
+	{
+		return List.of(new AggregationEdge(), new AssociationEdge(), new CallEdge(), new ConstructorEdge(), 
+				new DependencyEdge(), new GeneralizationEdge(), new ReturnEdge(), new StateTransitionEdge(), 
+				new UseCaseAssociationEdge(), new UseCaseDependencyEdge(), new UseCaseGeneralizationEdge());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideInvalidNodes")
+	void testInvalidElement_Node(Node pNode)
+	{
+		diagram().addRootNode(pNode);
+		assertFalse(aValidator.isValid());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideInvalidEdges")
+	void testInvalidElement_Edge(Edge pEdge)
+	{
+		pEdge.connect(aNoteNode, aNoteNode);
+		diagram().addEdge(pEdge);
+		diagram().addRootNode(aNoteNode);
+		assertFalse(aValidator.isValid());
+	}
 }
