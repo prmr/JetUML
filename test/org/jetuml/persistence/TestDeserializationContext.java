1c3f5587dde69c4e11a87c20a249bc345976d74d
Martin Robillard <prmr@gmx.com>
2023-06-27 13:13:09 -0400

    Issue #502 Add test for deserializer

--- test/org/jetuml/persistence/TestDeserializationContext.java
+++ test/org/jetuml/persistence/TestDeserializationContext.java
@@ -20,35 +20,31 @@
  *******************************************************************************/
 package org.jetuml.persistence;
 
+import static org.jetuml.testutils.CollectionAssertions.assertThat;
+import static org.jetuml.testutils.CollectionAssertions.hasElementsSameAs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jetuml.diagram.Diagram;
 import org.jetuml.diagram.DiagramType;
 import org.jetuml.diagram.Node;
 import org.jetuml.diagram.nodes.ClassNode;
-import org.junit.jupiter.api.BeforeEach;
+import org.jetuml.diagram.nodes.NoteNode;
 import org.junit.jupiter.api.Test;
 
 public class TestDeserializationContext
 {
-	private Diagram aDiagram;
+	private Diagram aDiagram = new Diagram(DiagramType.CLASS);
 	private DeserializationContext aContext;
-	private ClassNode aClassNode1; 
-	private ClassNode aClassNode2; 
-	private ClassNode aClassNode3; 
+	private ClassNode aClassNode1 = new ClassNode(); 
+	private ClassNode aClassNode2 = new ClassNode(); 
+	private ClassNode aClassNode3 = new ClassNode(); 
 
-	@BeforeEach
-	void setup()
-	{
-		aDiagram = new Diagram(DiagramType.CLASS);
-		aClassNode1 = new ClassNode();
-		aClassNode2 = new ClassNode();
-		aClassNode3 = new ClassNode();
-	}
-	
 	@Test
 	void testIdExists()
 	{
@@ -99,6 +95,32 @@ public class TestDeserializationContext
 		assertSame(aClassNode1, aContext.getNode(4));
 		assertSame(aClassNode2, aContext.getNode(1));
 		assertSame(aClassNode3, aContext.getNode(2));
+	}
+	
+	@Test
+	void testMaintainOrder()
+	{
+		Node node1 = new NoteNode();
+		Node node2 = new NoteNode();
+		Node node3 = new NoteNode();
+		aContext = new DeserializationContext(aDiagram);
+
+		aContext.addNode(aClassNode1, 0);
+		aContext.addNode(aClassNode2, 1);
+		aContext.addNode(aClassNode3, 2);
+		aContext.addNode(node1, 3);
+		aContext.addNode(node2, 4);
+		aContext.addNode(node3, 5);
+
+		assertThat(nodesInsideContext(), hasElementsSameAs, 
+				aClassNode1, aClassNode2, aClassNode3, node1, node2, node3);
+	}
+	
+	private List<Node> nodesInsideContext()
+	{
+		List<Node> result = new ArrayList<>();
+		aContext.forEach(node -> result.add(node));
+		return result;
 	}
 	
 	private int size()
