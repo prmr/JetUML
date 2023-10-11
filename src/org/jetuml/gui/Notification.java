2395a2e114f7417f2b804ef95851f9f7c159d5ce
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-10-11 20:41:05 +0200

    #508 Built Notification interface scaffolding

---
+++ src/org/jetuml/gui/Notification.java
@@ -0,0 +1,30 @@
+package org.jetuml.gui;
+
+/**
+ * An object that can be displayed by the NotificationHandler
+ */
+public interface Notification {
+
+    /**
+     * Show the Notification object to the desired coordinates.
+     *
+     * @param pX The target X position
+     * @param pY The target Y position
+     */
+    void show(double pX, double pY);
+
+    /**
+     * Move the Notification object to the desired X position.
+     *
+     * @param pX The target X position
+     */
+    void setX(double pX);
+
+    /**
+     * Move the Notification object to the desired Y position.
+     *
+     * @param pY The target Y position
+     */
+    void setY(double pY);
+
+}
