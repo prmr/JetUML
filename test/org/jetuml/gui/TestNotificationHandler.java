77cd97b7623567c26a47c6b69f350cb65a982c2f
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-10-23 21:56:04 -0400

    [#508] Fixed the NotificationHandler unit tests and added a new one

--- test/org/jetuml/gui/TestNotificationHandler.java
+++ test/org/jetuml/gui/TestNotificationHandler.java
@@ -25,25 +25,32 @@ import javafx.stage.Stage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
 public class TestNotificationHandler {
 
     private static Field aListField;
+    private static Stage aStage;
 
     @BeforeAll
     public static void setup()
     {
-        Platform.startup(() -> {});
         try {
             aListField = NotificationHandler.class.getDeclaredField("aNotificationList");
             aListField.setAccessible(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Platform.startup(() -> {
+            aStage = new Stage();
+            NotificationHandler.instance().setMainStage(aStage);
+        });
+
     }
 
     @AfterAll
@@ -57,54 +64,112 @@ public class TestNotificationHandler {
         }
     }
 
+    @BeforeEach
+    public void resetList() {
+        try
+        {
+            LinkedList<Notification> newList = new LinkedList<>();
+            aListField.set(NotificationHandler.instance(), newList);
+            NotificationHandler.instance().setMainStage(aStage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void waitForRunLater() throws InterruptedException {
+        Semaphore semaphore = new Semaphore(0);
+        Platform.runLater(() -> semaphore.release());
+        semaphore.acquire();
+    }
+
     @Test
     public void testSpawnNotifications()
     {
+        NotificationHandler handler = NotificationHandler.instance();
+
         Platform.runLater(() -> {
-            Stage stage;
-            stage = new Stage();
-            NotificationHandler.instance().setMainStage(stage);
-
-
-            NotificationHandler handler = NotificationHandler.instance();
-
             handler.spawn("This is a test error notification.", ToastNotification.Type.ERROR);
             handler.spawn("This is a test info notification.", ToastNotification.Type.INFO);
             handler.spawn("This is a test warning notification.", ToastNotification.Type.WARNING);
             handler.spawn("This is a test success notification.", ToastNotification.Type.SUCCESS);
-
-            LinkedList<Notification> notificationList;
-            try
-            {
-                notificationList = (LinkedList<Notification>) aListField.get(handler);
-                assertEquals(4, notificationList.size());
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
         });
+
+        try {
+            waitForRunLater();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        LinkedList<Notification> notificationList;
+        try
+        {
+            notificationList = (LinkedList<Notification>) aListField.get(handler);
+            assertEquals(4, notificationList.size());
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     public void testSpawnNotificationsWhenMainStageNotSet()
     {
+        NotificationHandler handler = NotificationHandler.instance();
+
         Platform.runLater(() -> {
-
-            NotificationHandler handler = NotificationHandler.instance();
-
+            handler.setMainStage(null);
             handler.spawn("This is a test error notification.", ToastNotification.Type.ERROR);
             handler.spawn("This is a test info notification.", ToastNotification.Type.INFO);
-
-            LinkedList<Notification> notificationList;
-            try
-            {
-                notificationList = (LinkedList<Notification>) aListField.get(handler);
-                assertEquals(0, notificationList.size());
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
         });
+
+        try {
+            waitForRunLater();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        LinkedList<Notification> notificationList;
+        try
+        {
+            notificationList = (LinkedList<Notification>) aListField.get(handler);
+            assertEquals(0, notificationList.size());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testNotificationCallback()
+    {
+
+        NotificationHandler handler = NotificationHandler.instance();
+
+        Platform.runLater(() ->
+        {
+            // This test notification has a 2-millisecond fade in animation, a 2-millisecond fade out animation and a lifespan of 100 ms.
+            ToastNotification notification = new ToastNotification("This is a test notification", ToastNotification.Type.INFO, handler.getMainStage(), 2, 2, 100);
+            handler.spawn(notification);
+        });
+
+        try {
+            waitForRunLater();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        LinkedList<Notification> notificationList;
+        try
+        {
+            notificationList = (LinkedList<Notification>) aListField.get(handler);
+            assertEquals(1, notificationList.size());
+            Thread.sleep(400);
+            assertEquals(0, notificationList.size());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 }
