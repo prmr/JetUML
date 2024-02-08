2364da61a86e341665fa3b5776f102b315fed9af
Junsoo Kim <junsookim047@gmail.com>
2024-02-08 10:08:14 -0500

    [prmr#528] Fix TypeNodeRenderer not displaying interface text on gui and add copyright information.

--- src/org/jetuml/rendering/CanvasFont.java
+++ src/org/jetuml/rendering/CanvasFont.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * JetUML - A desktop application for fast UML diagramming.
+ *
+ * Copyright (C) 2020, 2021 by McGill University.
+ *     
+ * See: https://github.com/prmr/JetUML
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *******************************************************************************/
 package org.jetuml.rendering;
 
 import org.jetuml.annotations.Singleton;
@@ -11,8 +31,8 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
 /**
- * Responsible for performing more rudimentary operations involving font,
- * as well as being synchronized with the user's current font.
+ * A singleton that is in sync with user font size setting and 
+ * manages font metrics calculations.
  */
 @Singleton
 public final class CanvasFont implements IntegerPreferenceChangeHandler
