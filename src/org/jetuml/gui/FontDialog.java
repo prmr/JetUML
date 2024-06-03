98bba7164704b8d7332336ff0233a2e75d9f1ccf
Junsoo Kim <junsookim047@gmail.com>
2024-06-03 11:03:30 -0400

    [#528] Remove CSS and Preview for FontDialog.

--- src/org/jetuml/gui/FontDialog.java
+++ src/org/jetuml/gui/FontDialog.java
@@ -35,7 +35,6 @@ import javafx.collections.FXCollections;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -45,7 +44,6 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -60,18 +58,14 @@ public class FontDialog
 	private static final int VSPACE = 20;
 	private static final Insets EXTRA_MARGIN = new Insets(0, 0, 0, 10);
 	
-	// The Serif and Monospaced fonts position the underscore character below the underline.
 	private static final ArrayList<String> FONT_FAMILIES = new ArrayList<>(Arrays.asList("System", "SansSerif", "Serif", "Monospaced"));
 	private static final ArrayList<Integer> FONT_SIZES = new ArrayList<>(
 			Arrays.asList(8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24));
-	private static final int PREVIEW_HEIGHT = 35;
-	private static final int PREVIEW_WIDTH = 110;
 	
 	private final Stage aStage = new Stage();
 	private final GridPane aLayout = new GridPane();
 	private final ComboBox<String> aFonts = new ComboBox<>(FXCollections.observableArrayList(FONT_FAMILIES));
 	private final ComboBox<Integer> aSizes = new ComboBox<>(FXCollections.observableArrayList(FONT_SIZES));
-	private final Label aPreview = new Label(RESOURCES.getString("dialog.font.preview"));
 	private final String aUserFamily = getCurrentFont();
 	private final int aUserSize = getCurrentFontSize();
 	
@@ -84,7 +78,6 @@ public class FontDialog
 	{
 		prepareStage(pOwner);
 		aStage.setScene(createScene());
-		aStage.getScene().getStylesheets().add(getClass().getResource("FontDialog.css").toExternalForm());
 	}
 	
 	private void prepareStage(Stage pOwner) 
@@ -115,7 +108,6 @@ public class FontDialog
 		aLayout.setPadding(new Insets(SPACING));
 		createFont();
 		createSize();
-		createPreview();
 		createButton();
 		return new Scene(aLayout);
 	}
@@ -150,26 +142,10 @@ public class FontDialog
 		aLayout.getChildren().addAll(size, aSizes);
 	}
 	
-	private void createPreview()
-	{
-		aPreview.setFont(Font.font(getCurrentFont(), getCurrentFontSize()));
-		aPreview.setPrefWidth(PREVIEW_WIDTH);
-		aPreview.setPrefHeight(PREVIEW_HEIGHT);
-		aPreview.setAlignment(Pos.CENTER);
-		
-		HBox previewContainer = new HBox(aPreview);
-		previewContainer.getStyleClass().add("font-preview");
-		previewContainer.setAlignment(Pos.CENTER);
-		GridPane.setConstraints(previewContainer, 0, 2, 2, 1, HPos.CENTER, VPos.CENTER);
-		
-		aLayout.getChildren().add(previewContainer);
-	}
-	
 	private void createButton()
 	{	
 		Button ok = new Button(RESOURCES.getString("dialog.font.ok"));
 		ok.setOnAction(pEvent -> aStage.close());
-		ok.getStyleClass().add("font-button");
 		
 		Button cancel = new Button(RESOURCES.getString("dialog.font.cancel"));
 		cancel.setOnAction(pEvent -> 
@@ -177,7 +153,6 @@ public class FontDialog
 			restoreUserSettings();
 			aStage.close();
 		});
-		cancel.getStyleClass().add("font-button");
 		
 		HBox layout = new HBox(SPACING);
 		layout.setAlignment(Pos.CENTER_RIGHT);
@@ -190,7 +165,6 @@ public class FontDialog
 			aFonts.getSelectionModel().select(DEFAULT_FONT_NAME);
 			aSizes.getSelectionModel().select((Integer) DEFAULT_FONT_SIZE);
 		});
-		restoreDefault.getStyleClass().add("font-button");
 		GridPane.setConstraints(restoreDefault, 0, 3);
 		
 		aLayout.getChildren().addAll(restoreDefault, layout);
@@ -223,7 +197,6 @@ public class FontDialog
 		{
 			UserPreferences.instance().setInteger(IntegerPreference.fontSize, aSizes.getSelectionModel().getSelectedItem());
 		}
-		aPreview.setFont(Font.font(getCurrentFont(), getCurrentFontSize()));
 	}
 	
 	/**
