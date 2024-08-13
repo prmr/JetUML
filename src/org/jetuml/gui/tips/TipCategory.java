7c181ab02b1949e5727a548724ea509a736dd075
Junsoo Kim <junsookim047@gmail.com>
2024-08-13 12:28:23 -0400

    [#530] Remove duplicate code.

--- src/org/jetuml/gui/tips/TipCategory.java
+++ src/org/jetuml/gui/tips/TipCategory.java
@@ -1,14 +1,18 @@
 package org.jetuml.gui.tips;
 
+import static org.jetuml.gui.tips.View.TOPIC;
+import static org.jetuml.gui.tips.View.LEVEL;
+import static org.jetuml.gui.tips.View.DIAGRAM;
+
 /**
  *  An enum declared with a View parameter to define
  *  all tip categories and the View that the category belongs to.
  */
 public enum TipCategory 
 {
-	CREATING(View.TOPIC), MODIFYING(View.TOPIC), SELECTING(View.TOPIC), COPYING(View.TOPIC), SEMANTICS(View.TOPIC), SETTINGS(View.TOPIC),
-	BEGINNER(View.LEVEL), INTERMEDIATE(View.LEVEL), ADVANCED(View.LEVEL),
-	CLASS(View.DIAGRAM), SEQUENCE(View.DIAGRAM), OBJECT(View.DIAGRAM), STATE(View.DIAGRAM), ALL(View.DIAGRAM);
+	CREATING(TOPIC), MODIFYING(TOPIC), SELECTING(TOPIC), COPYING(TOPIC), SEMANTICS(TOPIC), SETTINGS(TOPIC),
+	BEGINNER(LEVEL), INTERMEDIATE(LEVEL), ADVANCED(LEVEL),
+	CLASS(DIAGRAM), SEQUENCE(DIAGRAM), OBJECT(DIAGRAM), STATE(DIAGRAM), ALL(DIAGRAM);
 	
 	private final View aView;
 	
