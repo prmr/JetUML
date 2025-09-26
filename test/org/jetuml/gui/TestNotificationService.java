12bebda711a0688ae89cdb821d0ef98e72ee1833
Martin Robillard <prmr@gmx.com>
2025-09-26 08:43:32 -0400

    [#573] Eliminate JUnit warnings about private methods

--- test/org/jetuml/gui/TestNotificationService.java
+++ test/org/jetuml/gui/TestNotificationService.java
@@ -36,220 +36,234 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
-public class TestNotificationService 
-{
+public class TestNotificationService {
 	/* The aNotifications field in NotificationService. */
-    private static Field aNotifications;
-    
-    /* The aStage field in ToastNotification. */ 
-    private static Field aStageField;
-    
-    /* A working stage used for testing. */
-    private static Stage aStage;
+	private static Field aNotifications;
 
-    /*
-     * Allows waiting for the platform instructions to be executed before continuing on.
-     * It then prevents having a test finishing before the JavaFX instructions are run.
-     */
-    private static void waitForRunLater() throws InterruptedException
-    {
-        Semaphore semaphore = new Semaphore(0);
-        Platform.runLater(semaphore::release);
-        semaphore.acquire();
-    }
+	/* The aStage field in ToastNotification. */
+	private static Field aStageField;
 
-    @BeforeAll
-    private static void setup() throws ReflectiveOperationException, InterruptedException
-    {
-        aNotifications = NotificationService.class.getDeclaredField("aNotifications");
-        aNotifications.setAccessible(true);
-        aStageField = ToastNotification.class.getDeclaredField("aStage");
-        aStageField.setAccessible(true);
+	/* A working stage used for testing. */
+	private static Stage aStage;
 
-        // Manually setting the parent stage of the NotificationService
-        JavaFXLoader.load();
-        Platform.runLater(() -> {
-            aStage = new Stage();
-            NotificationService.instance().setMainStage(aStage);
-            aStage.show();
-        });
-        waitForRunLater();
-    }
+	/*
+	 * Allows waiting for the platform instructions to be executed before continuing
+	 * on. It then prevents having a test finishing before the JavaFX instructions
+	 * are run.
+	 */
+	private static void waitForRunLater() throws InterruptedException {
+		Semaphore semaphore = new Semaphore(0);
+		Platform.runLater(semaphore::release);
+		semaphore.acquire();
+	}
 
-    @AfterAll
-    private static void closeStage() throws InterruptedException {
-        Platform.runLater(aStage::close);
-        waitForRunLater();
-        NotificationService.instance().setMainStage(null);
-    }
+	@BeforeAll
+	static void setup() throws ReflectiveOperationException, InterruptedException {
+		aNotifications = NotificationService.class.getDeclaredField("aNotifications");
+		aNotifications.setAccessible(true);
+		aStageField = ToastNotification.class.getDeclaredField("aStage");
+		aStageField.setAccessible(true);
 
-    @SuppressWarnings("static-method") // Cannot be statis as per JUnit
+		// Manually setting the parent stage of the NotificationService
+		JavaFXLoader.load();
+		Platform.runLater(() -> {
+			aStage = new Stage();
+			NotificationService.instance().setMainStage(aStage);
+			aStage.show();
+		});
+		waitForRunLater();
+	}
+
+	@AfterAll
+	static void closeStage() throws InterruptedException {
+		Platform.runLater(aStage::close);
+		waitForRunLater();
+		NotificationService.instance().setMainStage(null);
+	}
+
 	@BeforeEach
-    private void resetListAndProperties() throws ReflectiveOperationException
-    {
-        ArrayList<Notification> newList = new ArrayList<>();
-        aNotifications.set(NotificationService.instance(), newList);
+	void resetListAndProperties() throws ReflectiveOperationException {
+		ArrayList<Notification> newList = new ArrayList<>();
+		aNotifications.set(NotificationService.instance(), newList);
 
-        aStage.setHeight(695.5);
-        aStage.setWidth(1125.0);
-        aStage.setX(220.5);
-        aStage.setY(220.5);
-    }
+		aStage.setHeight(695.5);
+		aStage.setWidth(1125.0);
+		aStage.setX(220.5);
+		aStage.setY(220.5);
+	}
 
-    @Test
-    void testSpawnToast() throws InterruptedException, ReflectiveOperationException
-    {
-        Platform.runLater(() -> {
-        	NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-        	NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        	NotificationService.instance().spawnNotification("This is a test warning notification.", ToastNotification.Type.WARNING);
-        	NotificationService.instance().spawnNotification("This is a test success notification.", ToastNotification.Type.SUCCESS);
-        });
+	@Test
+	void testSpawnToast() throws InterruptedException, ReflectiveOperationException {
+		Platform.runLater(() -> {
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+			NotificationService.instance().spawnNotification("This is a test warning notification.",
+					ToastNotification.Type.WARNING);
+			NotificationService.instance().spawnNotification("This is a test success notification.",
+					ToastNotification.Type.SUCCESS);
+		});
 
-        waitForRunLater();
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
+		@SuppressWarnings("unchecked")
 		List<Notification> notificationList = (List<Notification>) aNotifications.get(NotificationService.instance());
-        assertEquals(4, notificationList.size());
-    }
+		assertEquals(4, notificationList.size());
+	}
 
-    /*
-        Notifications should stack, one on top of the other.
-     */
-    @Test
-    void testNotificationPosition() throws InterruptedException, ReflectiveOperationException
-    {
-        Platform.runLater(() -> {
-        	NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-        	NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        	NotificationService.instance().spawnNotification("This is a test warning notification.", ToastNotification.Type.WARNING);
-        });
-        waitForRunLater();
+	/*
+	 * Notifications should stack, one on top of the other.
+	 */
+	@Test
+	void testNotificationPosition() throws InterruptedException, ReflectiveOperationException {
+		Platform.runLater(() -> {
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+			NotificationService.instance().spawnNotification("This is a test warning notification.",
+					ToastNotification.Type.WARNING);
+		});
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
-		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications.get(NotificationService.instance());
+		@SuppressWarnings("unchecked")
+		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications
+				.get(NotificationService.instance());
 
-        Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
-        Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
-        Stage stage3 = (Stage) aStageField.get(notificationList.get(2));
+		Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
+		Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
+		Stage stage3 = (Stage) aStageField.get(notificationList.get(2));
 
-        assertTrue(stage1.getY() < stage2.getY());
-        assertTrue(stage2.getY() < stage3.getY());
-    }
+		assertTrue(stage1.getY() < stage2.getY());
+		assertTrue(stage2.getY() < stage3.getY());
+	}
 
-    /*
-        If the stage is moved, the notifications should move as well.
-     */
-    @Test
-    void testNotificationPositionWhenStageMoved() throws InterruptedException, ReflectiveOperationException
-    {
-        aStage.setX(0.5);
-        Platform.runLater(() -> {
-        	NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-        	NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        });
-        waitForRunLater();
+	/*
+	 * If the stage is moved, the notifications should move as well.
+	 */
+	@Test
+	void testNotificationPositionWhenStageMoved() throws InterruptedException, ReflectiveOperationException {
+		aStage.setX(0.5);
+		Platform.runLater(() -> {
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+		});
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
-		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications.get(NotificationService.instance());
+		@SuppressWarnings("unchecked")
+		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications
+				.get(NotificationService.instance());
 
-        Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
-        Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
+		Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
+		Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
 
-        double stage1X = stage1.getX();
-        double stage2X = stage2.getX();
+		double stage1X = stage1.getX();
+		double stage2X = stage2.getX();
 
-        aStage.setX(200);
+		aStage.setX(200);
 
-        assertTrue(stage1.getX() > stage1X);
-        assertTrue(stage2.getX() > stage2X);
+		assertTrue(stage1.getX() > stage1X);
+		assertTrue(stage2.getX() > stage2X);
 
-        assertTrue(stage1.getY() < stage2.getY());
-    }
+		assertTrue(stage1.getY() < stage2.getY());
+	}
 
-    @Test
-    void testNotificationPositionWhenStageResized() throws InterruptedException, ReflectiveOperationException
-    {
-        aStage.setHeight(600);
-        Platform.runLater(() -> {
-        	NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-        	NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        });
-        waitForRunLater();
+	@Test
+	void testNotificationPositionWhenStageResized() throws InterruptedException, ReflectiveOperationException {
+		aStage.setHeight(600);
+		Platform.runLater(() -> {
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+		});
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
-        ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications.get(NotificationService.instance());
+		@SuppressWarnings("unchecked")
+		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications
+				.get(NotificationService.instance());
 
-        Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
-        Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
+		Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
+		Stage stage2 = (Stage) aStageField.get(notificationList.get(1));
 
-        double stage1Y = stage1.getY();
-        double stage2Y = stage2.getY();
+		double stage1Y = stage1.getY();
+		double stage2Y = stage2.getY();
 
-        aStage.setHeight(300);
+		aStage.setHeight(300);
 
-        assertNotEquals(stage1.getY(), stage1Y);
-        assertNotEquals(stage2.getY(), stage2Y);
+		assertNotEquals(stage1.getY(), stage1Y);
+		assertNotEquals(stage2.getY(), stage2Y);
 
-        assertTrue(stage1.getY() < stage2.getY());
-    }
+		assertTrue(stage1.getY() < stage2.getY());
+	}
 
-    @Test
-    void testNotificationPositionWhenRemoval() throws InterruptedException, ReflectiveOperationException
-    {
-        Platform.runLater(() -> {
-        	NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-        	NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        });
-        waitForRunLater();
+	@Test
+	void testNotificationPositionWhenRemoval() throws InterruptedException, ReflectiveOperationException {
+		Platform.runLater(() -> {
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+		});
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
-		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications.get(NotificationService.instance());
+		@SuppressWarnings("unchecked")
+		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications
+				.get(NotificationService.instance());
 
-        Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
+		Stage stage1 = (Stage) aStageField.get(notificationList.get(0));
 
-        double stage1Y = stage1.getY();
+		double stage1Y = stage1.getY();
 
-        notificationList.remove(1);
-        NotificationService.instance().updateNotificationPosition();
+		notificationList.remove(1);
+		NotificationService.instance().updateNotificationPosition();
 
-        assertTrue(stage1.getY() > stage1Y);
-    }
+		assertTrue(stage1.getY() > stage1Y);
+	}
 
-    /*
-        If the notifications overshoot the main window, the oldest ones are removed to keep
-        the whole notification stack in the frame.
-     */
-    @Test
-    void testNotificationDeletionWhenWindowBorderReached() throws InterruptedException, ReflectiveOperationException
-    {
-        Field notificationSpacingField = NotificationService.class.getDeclaredField("NOTIFICATION_DISPLAY_SPACING");
-        notificationSpacingField.setAccessible(true);
-        Field notificationYMarginField = NotificationService.class.getDeclaredField("NOTIFICATION_DISPLAY_Y_MARGIN");
-        notificationYMarginField.setAccessible(true);
+	/*
+	 * If the notifications overshoot the main window, the oldest ones are removed
+	 * to keep the whole notification stack in the frame.
+	 */
+	@Test
+	void testNotificationDeletionWhenWindowBorderReached() throws InterruptedException, ReflectiveOperationException {
+		Field notificationSpacingField = NotificationService.class.getDeclaredField("NOTIFICATION_DISPLAY_SPACING");
+		notificationSpacingField.setAccessible(true);
+		Field notificationYMarginField = NotificationService.class.getDeclaredField("NOTIFICATION_DISPLAY_Y_MARGIN");
+		notificationYMarginField.setAccessible(true);
 
-        int notificationSpacing = (int) notificationSpacingField.get(null);
-        int yMargin = (int) notificationYMarginField.get(null);
+		int notificationSpacing = (int) notificationSpacingField.get(null);
+		int yMargin = (int) notificationYMarginField.get(null);
 
-        Platform.runLater(() -> {
-            ToastNotification errorNotification = new ToastNotification("This is a test error notification.", ToastNotification.Type.ERROR, aStage);
-            ToastNotification infoNotification = new ToastNotification("This is a test info notification.", ToastNotification.Type.INFO, aStage);
+		Platform.runLater(() -> {
+			ToastNotification errorNotification = new ToastNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR, aStage);
+			ToastNotification infoNotification = new ToastNotification("This is a test info notification.",
+					ToastNotification.Type.INFO, aStage);
 
-            NotificationService.instance().spawnNotification(errorNotification);
-            NotificationService.instance().spawnNotification(infoNotification);
+			NotificationService.instance().spawnNotification(errorNotification);
+			NotificationService.instance().spawnNotification(infoNotification);
 
-            // Let's compute the necessary height of the main window to fit two notifications
-            double height = yMargin + notificationSpacing*2 + errorNotification.getHeight() + infoNotification.getHeight()+1;
-            aStage.setHeight(height);
+			// Let's compute the necessary height of the main window to fit two
+			// notifications
+			double height = yMargin + notificationSpacing * 2 + errorNotification.getHeight()
+					+ infoNotification.getHeight() + 1;
+			aStage.setHeight(height);
 
-            NotificationService.instance().spawnNotification("This is a test error notification.", ToastNotification.Type.ERROR);
-            NotificationService.instance().spawnNotification("This is a test info notification.", ToastNotification.Type.INFO);
-        });
-        waitForRunLater();
+			NotificationService.instance().spawnNotification("This is a test error notification.",
+					ToastNotification.Type.ERROR);
+			NotificationService.instance().spawnNotification("This is a test info notification.",
+					ToastNotification.Type.INFO);
+		});
+		waitForRunLater();
 
-        @SuppressWarnings("unchecked")
-        ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications.get(NotificationService.instance());
+		@SuppressWarnings("unchecked")
+		ArrayList<Notification> notificationList = (ArrayList<Notification>) aNotifications
+				.get(NotificationService.instance());
 
-        assertEquals(2, notificationList.size());
-    }
+		assertEquals(2, notificationList.size());
+	}
 }
